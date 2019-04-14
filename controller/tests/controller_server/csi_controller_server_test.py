1266b4aa4fceacab0106caf052481d12f03b32f3
olgasht <olgasht@il.ibm.com>
2019-04-14 10:50:25 +0300

    CSI-11: added test settings- to test

--- controller/tests/controller_server/csi_controller_server_test.py
+++ controller/tests/controller_server/csi_controller_server_test.py
@@ -5,7 +5,7 @@ from mock import patch, Mock
 from controller.csi_general import csi_pb2
 from controller.array_action.array_mediator_xiv import XIVArrayMediator
 from controller.controller_server.csi_controller_server import ControllerServicer
-from controller.controller_server.settings import vol_name
+from controller.controller_server.test_settings import vol_name
 
 
 class TestControllerServer(unittest.TestCase):
@@ -17,7 +17,7 @@ class TestControllerServer(unittest.TestCase):
         self.mediator = XIVArrayMediator("user", "password", self.fqdn) 
         self.mediator.client = Mock()
         self.servicer = ControllerServicer(self.fqdn) 
-          
+
     @patch("controller.array_action.array_connection_manager.ArrayConnectionManager.__enter__")
     @patch("controller.array_action.array_connection_manager.ArrayConnectionManager.__exit__")
     def test_create_volume_with_empty_name(self, a_enter, a_exit):
@@ -28,7 +28,7 @@ class TestControllerServer(unittest.TestCase):
         res = self.servicer.CreateVolume(request, context)
         context.set_code.assert_called_once_with(grpc.StatusCode.INVALID_ARGUMENT)
         self.assertEqual(res, csi_pb2.CreateVolumeResponse())
-         
+
     @patch("controller.array_action.array_connection_manager.ArrayConnectionManager.__enter__")
     @patch("controller.array_action.array_connection_manager.ArrayConnectionManager.__exit__")
     def test_create_volume_succeeds(self, a_exit, a_enter):
@@ -40,7 +40,7 @@ class TestControllerServer(unittest.TestCase):
         res = self.servicer.CreateVolume(request, context)
         context.set_code.assert_called_once_with(grpc.StatusCode.OK)
         self.mediator.get_volume.assert_called_once_with(vol_name)
-     
+
     @patch("controller.array_action.array_mediator_xiv.XIVArrayMediator.get_volume")
     @patch("controller.array_action.array_connection_manager.ArrayConnectionManager.__enter__")
     def test_create_volume_exception(self, a_enter, get_volume):
@@ -54,4 +54,3 @@ class TestControllerServer(unittest.TestCase):
         print res
         context.set_code.assert_called_once_with(grpc.StatusCode.INTERNAL)
         self.mediator.get_volume.assert_called_once_with(vol_name)
-          
