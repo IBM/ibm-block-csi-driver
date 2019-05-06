b2f7383e5771a4c5460d1773a311d5c0f48c9da2
olgasht <olgasht@il.ibm.com>
2019-05-06 14:05:56 +0300

    CSI-56: added full exception path

--- controller/array_action/array_mediator_xiv.py
+++ controller/array_action/array_mediator_xiv.py
@@ -3,7 +3,7 @@ from pyxcli import errors as xcli_errors
 from controller.common.csi_logger import get_stdout_logger
 from array_mediator_interface import ArrayMediator
 from array_action_types import Volume
-from errors import CredentialsError, VolumeNotFoundError, IllegalObjectName, PoolDoesNotMatchCapabilities, \
+from controller.array_action.errors import CredentialsError, VolumeNotFoundError, IllegalObjectName, PoolDoesNotMatchCapabilities, \
     CapabilityNotSupported, \
     VolumeAlreadyExists, PoolDoesNotExist
 
