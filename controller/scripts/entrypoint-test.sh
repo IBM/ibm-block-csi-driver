65fca2a092cfb1c70ae3841a9bfb82231a532380
Shay Berman <shaybery@gmail.com>
2019-05-08 12:18:47 +0300

    entrypoint-test.sh if coverage dir not exist just create it for local run UT

--- controller/scripts/entrypoint-test.sh
+++ controller/scripts/entrypoint-test.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -x
-exec /bin/nosetests --with-coverage --cover-xml --cover-xml-file=/driver/coverage/.coverage.xml --with-xunit --xunit-file=/driver/coverage/.unitests.xml $@
+coveragedir=/driver/coverage/
+[ ! -d $coveragedir ] && mkdir -p $coveragedir
+exec /bin/nosetests --with-coverage --cover-xml --cover-xml-file=$coveragedir/.coverage.xml --with-xunit --xunit-file=$coveragedir/.unitests.xml $@
