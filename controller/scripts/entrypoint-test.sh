b6eb54d9dda35a931fc6d6eb4cdf66835c80ef56
Shay Berman <shaybery@gmail.com>
2019-05-08 10:33:31 +0300

    add xunit for UT, add collection of reports in pipeline

--- controller/scripts/entrypoint-test.sh
+++ controller/scripts/entrypoint-test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-exec /bin/nosetests --with-coverage --cover-xml --cover-xml-file=/driver/coverage/.coverage $@
+exec /bin/nosetests --with-coverage --cover-xml --cover-xml-file=/driver/coverage/.coverage.xml --with-xunit --xunit-file=/driver/coverage/.unitests.xml $@
