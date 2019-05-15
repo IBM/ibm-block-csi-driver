dc8da2340ab87c0f4defa4a216c84b173442e743
Shay Berman <shaybery@gmail.com>
2019-05-15 09:51:20 +0300

    add comment for SVC

--- controller/array_action/array_mediator_svc.py
+++ controller/array_action/array_mediator_svc.py
@@ -1,7 +1,7 @@
 from array_mediator_interface import  ArrayMediator
 
 class SVCArrayMediator(ArrayMediator):
-    max_connections = 10
+    max_connections = 10  # TODO : need to implement all the interface methods\properties 
     port = 22
     array_type = "svc"
     pass
