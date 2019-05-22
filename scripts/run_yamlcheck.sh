f394d8d11da97c17edb96713a9e1ae740e17ac45
Shay Berman <shaybery@gmail.com>
2019-05-22 12:47:01 +0300

    forgot to rename 1.14 in the yml check

--- scripts/run_yamlcheck.sh
+++ scripts/run_yamlcheck.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-docker run -t -v `pwd`/deploy/kubernetes/v1.13:/deploy/kubernetes/v1.13 garethr/kubeval deploy/kubernetes/v1.13/*.yaml
+docker run -t -v `pwd`/deploy/kubernetes/v1.14:/deploy/kubernetes/v1.14 garethr/kubeval deploy/kubernetes/v1.14/*.yaml
