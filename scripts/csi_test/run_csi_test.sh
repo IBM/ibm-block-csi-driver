00abe941f676d0c27c3e328474a7032a068adefd
olgasht <olgasht@il.ibm.com>
2019-06-04 18:28:14 +0300

    CSI-101: added somce changes

--- scripts/csi_test/run_csi_test.sh
+++ scripts/csi_test/run_csi_test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
- 
-/usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity  --csi.endpoint $1 --csi.secrets $2 --csi.testvolumeparameters $3 --ginkgo.focus $4
 echo "usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity  --csi.endpoint $1 --csi.secrets $2 --csi.testvolumeparameters $3 --ginkgo.focus $4"
-#./cmd/csi-sanity/csi-sanity  --csi.endpoint /tmp/b --csi.secrets olga_secrets --csi.testvolumeparameters olga_params --ginkgo.focus "ValidateVolumeCapabilities|ControllerPublishVolume"
+/usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity  --csi.endpoint $1 --csi.secrets $2 --csi.testvolumeparameters $3 --ginkgo.focus $4
+
 
 
