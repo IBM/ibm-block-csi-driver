ac0fd2731877f392463fe6735534eb2ebac8a4c3
root <root@olgas-kub-19-master.ps.xiv.ibm.com>
2019-06-04 16:19:16 +0300

    CSI-101: remove double csi-controller name

--- scripts/ci/build_and_run_controller_server.sh
+++ scripts/ci/build_and_run_controller_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
-docker build  -f Dockerfile-csi-controller csi-controller $1 . &&  docker run  --name $1  csi-controller -e [::]:4444
+docker build  -f Dockerfile-csi-controller csi-controller . &&  docker run  --name $1  csi-controller -e [::]:4444
 
