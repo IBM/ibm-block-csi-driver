530fb75805d2015174e8509766877fc34c278276
root <root@olgas-kub-19-master.ps.xiv.ibm.com>
2019-06-04 16:22:03 +0300

    CSI-101: fixes

--- scripts/ci/build_and_run_controller_server.sh
+++ scripts/ci/build_and_run_controller_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
-docker build  -f Dockerfile-csi-controller csi-controller . &&  docker run  --name $1  csi-controller -e [::]:4444
+docker build  -f Dockerfile-csi-controller -t csi-controller . &&  docker run  --name $1  csi-controller -e [::]:4444
 
