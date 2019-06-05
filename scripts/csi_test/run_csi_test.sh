f0ad307101312d683c078566a220fb98b19c83d4
olgasht <olgasht@il.ibm.com>
2019-06-05 13:05:52 +0300

    CSI-101: added somce changes

--- scripts/csi_test/run_csi_test.sh
+++ scripts/csi_test/run_csi_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity  --csi.endpoint $1 --csi.secrets $2 --csi.testvolumeparameters $3 --ginkgo.focus $4"
-/usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity  --csi.endpoint $1 --csi.secrets $2 --csi.testvolumeparameters $3 --ginkgo.focus $4
+/usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity  --csi.endpoint $1 --csi.secrets $2 --csi.testvolumeparameters $3  --csi.junitfile $5 --ginkgo.focus $4
 
 
 
