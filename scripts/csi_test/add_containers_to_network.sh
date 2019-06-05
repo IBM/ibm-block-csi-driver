150b0ad05cf64a64db0b3d08b2c0bc323fa8dfcb
olgasht <olgasht@il.ibm.com>
2019-06-05 15:00:25 +0300

    CSI-101: added somce changes

--- scripts/csi_test/add_containers_to_network.sh
+++ scripts/csi_test/add_containers_to_network.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
+
+
 docker network create testnet
+
+
+a=`docker ps -f "name=$1" | wc -l`
+while [ a != '2' ]
+do
+    sleep 2
+    echo "docker ps -f \"name=$1\" | wc -l"
+    a=`docker ps -f "name=$1" | wc -l`
+done
+
 
 docker network connect testnet $1 --alias server 
 echo "docker network connect testnet $1 --alias server"
-docker network connect testnet $2 --alias client 
+
+a=`docker ps -f "name=$2" | wc -l`
+while [ a != '2' ]
+do
+    sleep 2
+    echo "docker ps -f \"name=$2\" | wc -l"
+    a=`docker ps -f "name=$2" | wc -l`
+done
+
+
+docker network connect testnet $2 --alias client
 echo "docker network connect testnet $2 --alias client"
