56a730d1a3fece6067a8217b10c2efb2a2f2cac1
root <root@olgas-kub-19-master.ps.xiv.ibm.com>
2019-06-11 15:23:24 +0300

    CSI-101: changes

--- scripts/csi_test/run_csi_test.sh
+++ scripts/csi_test/run_csi_test.sh
@@ -4,9 +4,9 @@
 echo "usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity  --csi.endpoint $1 --csi.secrets $2 --csi.testvolumeparameters $3 --ginkgo.focus $4"
 
 # update CSI secret
-sed -i -e 's/MGMT_ADDRESS/${MGMT_ADDRESS}/g' $2
-sed -i -e 's/USERNAME/${USERNAME}/g' $2
-sed -i -e 's/PASSWORD/${PASSWORD}/g' $2
+sed -i -e "s/MGMT_ADDRESS/${MGMT_ADDRESS}/g" $2
+sed -i -e "s/USERNAME/${USERNAME}/g" $2
+sed -i -e "s/PASSWORD/${PASSWORD}/g" $2
 
 echo "####### ${MGMT_ADDRESS} ${USERNAME} ${PASSWORD} "
 cat $2
