f00b987c11f2aea0a55537f84c9959cbe42ed00e
root <root@olgas-kub-19-master.ps.xiv.ibm.com>
2019-06-11 19:41:36 +0300

    CSI-101: changes

--- scripts/ci/build_and_run_controller_server.sh
+++ scripts/ci/build_and_run_controller_server.sh
@@ -2,5 +2,5 @@
 
 #docker build  -f Dockerfile-csi-controller -t csi-controller . &&  docker run  --name $1  csi-controller -e [::]:4444
 
-docker build  -f Dockerfile-csi-controller -t csi-controller . &&  docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw  --name $1  csi-controller -e /tmp/k8s_dir/f
+docker build  -f Dockerfile-csi-controller -t csi-controller . &&  docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw  --name $1  csi-controller -e unix://tmp/k8s_dir/f
 
