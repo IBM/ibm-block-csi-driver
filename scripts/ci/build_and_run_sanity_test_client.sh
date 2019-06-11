cb2878954159b93a6bff380629d619e0c26eb454
root <root@olgas-kub-19-master.ps.xiv.ibm.com>
2019-06-11 15:00:31 +0300

    CSI-101: changes

--- scripts/ci/build_and_run_sanity_test_client.sh
+++ scripts/ci/build_and_run_sanity_test_client.sh
@@ -2,4 +2,6 @@
 
 #docker build  -f Dockerfile-csi-test -t csi-sanity-test . && docker run  --rm  --name $1 csi-sanity-test
 
+echo "@@@###@@@ ${MGMT_ADDRESS} ${USERNAME} ${PASSWORD}"
+
 docker build  -f Dockerfile-csi-test -t csi-sanity-test . && docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw --rm  --name $1 csi-sanity-test
