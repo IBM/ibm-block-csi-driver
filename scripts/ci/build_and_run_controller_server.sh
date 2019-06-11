d3e771ef7ca6e82bcb95dc6ca8851a3b7b0bd72d
root <root@olgas-kub-19-master.ps.xiv.ibm.com>
2019-06-11 11:38:20 +0300

    CSI-101: trying to use dirs

--- scripts/ci/build_and_run_controller_server.sh
+++ scripts/ci/build_and_run_controller_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -xe
 
-docker build  -f Dockerfile-csi-controller -t csi-controller . &&  docker run  --name $1  csi-controller -e [::]:4444
+#docker build  -f Dockerfile-csi-controller -t csi-controller . &&  docker run  --name $1  csi-controller -e [::]:4444
+
+docker build  -f Dockerfile-csi-controller -t csi-controller . &&  docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw  --name $1  csi-controller -e /tmp/k8s_dir/f
 
--- scripts/ci/build_and_run_sanity_test_client.sh
+++ scripts/ci/build_and_run_sanity_test_client.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -xe
 
-docker build  -f Dockerfile-csi-test -t csi-sanity-test . && docker run  --rm  --name $1 csi-sanity-test
+#docker build  -f Dockerfile-csi-test -t csi-sanity-test . && docker run  --rm  --name $1 csi-sanity-test
+
+docker build  -f Dockerfile-csi-test -t csi-sanity-test . && docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw --rm  --name $1 csi-sanity-test
