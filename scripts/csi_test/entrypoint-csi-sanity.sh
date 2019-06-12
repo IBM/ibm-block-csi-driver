3d4438e958880b0614242877664fdf53cbc508da
olgashtivelman <olgasht@il.ibm.com>
2019-06-12 15:59:28 +0300

    CSI-101: add tests to run param

--- scripts/csi_test/entrypoint-csi-sanity.sh
+++ scripts/csi_test/entrypoint-csi-sanity.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-#### PARAMETERS
-# $1 - endpoint
-# $2 - secret
-# $3 - parameter file
-# $4 - junit output
-# $5 - tests to run file
-
 echo "JUNIT OUTPUT"
 echo ${JUNIT_OUTPUT} ${SECRET_FILE} ${PARAM_FILE} ${ENDPOINT} ${TESTS_TO_RUN_FILE}
 
@@ -20,6 +13,5 @@ sed -i -e "s/POOL_NAME/${POOL_NAME}/g" ${PARAM_FILE}
 
 # get tests to run
 TESTS=`cat ${TESTS_TO_RUN_FILE}| tr '\n' "|"`
-
 
 /usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity  --csi.endpoint ${ENDPOINT} --csi.secrets ${SECRET_FILE} --csi.testvolumeparameters ${PARAM_FILE}  --csi.junitfile ${JUNIT_OUTPUT} --ginkgo.focus ${TESTS}
