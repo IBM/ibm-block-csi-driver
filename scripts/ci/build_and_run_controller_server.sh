934102f2f864bef469172f326de8be0167b1cfb1
olgashtivelman <olgasht@il.ibm.com>
2019-06-12 14:23:30 +0300

    CSI-101: add tests to run param

--- scripts/ci/build_and_run_controller_server.sh
+++ scripts/ci/build_and_run_controller_server.sh
@@ -8,5 +8,5 @@ fi
 
 chmod 777 /tmp/k8s_dir
 
-docker build --no-cache -f Dockerfile-csi-controller -t csi-controller . &&  docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw  --rm --name $1  csi-controller -e unix://tmp/k8s_dir/f
+docker build  -f Dockerfile-csi-controller -t csi-controller . &&  docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw  --rm --name $1  csi-controller -e unix://tmp/k8s_dir/f
 
--- scripts/ci/build_and_run_sanity_test_client.sh
+++ scripts/ci/build_and_run_sanity_test_client.sh
@@ -5,4 +5,4 @@
 
 echo "@@@###@@@ ${MGMT_ADDRESS} ${USERNAME} ${PASSWORD} ${TESTS_TO_RUN} "
 
-docker build --no-cache --build-arg MGMT_ADDRESS=${MGMT_ADDRESS} --build-arg USERNAME=${USERNAME} --build-arg PASSWORD=${PASSWORD} --build-arg TESTS_TO_RUN=${TESTS_TO_RUN}  -f Dockerfile-csi-test -t csi-sanity-test . && docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -v$2:/tmp/test_results:rw --rm  --name $1 csi-sanity-test
+docker build  --build-arg MGMT_ADDRESS=${MGMT_ADDRESS} --build-arg USERNAME=${USERNAME} --build-arg PASSWORD=${PASSWORD} --build-arg TESTS_TO_RUN=${TESTS_TO_RUN}  -f Dockerfile-csi-test -t csi-sanity-test . && docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -v$2:/tmp/test_results:rw --rm  --name $1 csi-sanity-test
