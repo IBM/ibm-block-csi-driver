1994738a23c0a8800c87bd9df6138e1ea5037f7f
olgashtivelman <olgasht@il.ibm.com>
2019-06-12 15:47:50 +0300

    CSI-101: add tests to run param

--- scripts/csi_test/entrypoint-csi-sanity.sh
+++ scripts/csi_test/entrypoint-csi-sanity.sh
@@ -7,18 +7,19 @@
 # $4 - junit output
 # $5 - tests to run file
 
+echo "JUNIT OUTPUT"
+echo ${JUNIT_OUTPUT} ${SECRET_FILE} ${PARAM_FILE} ${ENDPOINT} ${TESTS_TO_RUN_FILE}
+
 # update CSI secret
-sed -i -e "s/MGMT_ADDRESS/${MGMT_ADDRESS}/g" $2
-sed -i -e "s/USERNAME/${USERNAME}/g" $2
-sed -i -e "s/PASSWORD/${PASSWORD}/g" $2
+sed -i -e "s/MGMT_ADDRESS/${MGMT_ADDRESS}/g" ${SECRET_FILE}
+sed -i -e "s/USERNAME/${USERNAME}/g" ${SECRET_FILE}
+sed -i -e "s/PASSWORD/${PASSWORD}/g" ${SECRET_FILE}
 
 # update params file
-sed -i -e "s/POOL_NAME/${POOL_NAME}/g" $3
+sed -i -e "s/POOL_NAME/${POOL_NAME}/g" ${PARAM_FILE}
 
 # get tests to run
-test=`cat $5| tr '\n' "|"`
+TESTS=`cat ${TESTS_TO_RUN_FILE}| tr '\n' "|"`
 
-echo "JUNIT OUTPUT"
-echo ${JUNIT_OUTPUT}
 
-/usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity  --csi.endpoint $1 --csi.secrets $2 --csi.testvolumeparameters $3  --csi.junitfile $4 --ginkgo.focus ${tests}
+/usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity  --csi.endpoint ${ENDPOINT} --csi.secrets ${SECRET_FILE} --csi.testvolumeparameters ${PARAM_FILE}  --csi.junitfile ${JUNIT_OUTPUT} --ginkgo.focus ${TESTS}
