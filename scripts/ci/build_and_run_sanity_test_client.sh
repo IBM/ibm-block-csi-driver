009680b78d5f4c075c09b1e520d657e204a24cfa
olgashtivelman <olgasht@il.ibm.com>
2019-06-12 14:49:59 +0300

    CSI-101: add tests to run param

--- scripts/ci/build_and_run_sanity_test_client.sh
+++ scripts/ci/build_and_run_sanity_test_client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 
-docker build  --build-arg MGMT_ADDRESS=${MGMT_ADDRESS} --build-arg USERNAME=${USERNAME} --build-arg PASSWORD=${PASSWORD} --build-arg TESTS_TO_RUN=${TESTS_TO_RUN}  -f Dockerfile-csi-test -t csi-sanity-test . && docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -v$2:/tmp/test_results:rw --rm  --name $1 csi-sanity-test
+docker build  --build-arg MGMT_ADDRESS=${MGMT_ADDRESS} --build-arg USERNAME=${USERNAME} --build-arg PASSWORD=${PASSWORD} --build-arg TESTS_TO_RUN=${TESTS_TO_RUN}  --build-arg POOL_NAME=${POOL_NAME} -f Dockerfile-csi-test -t csi-sanity-test . && docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -v$2:/tmp/test_results:rw --rm  --name $1 csi-sanity-test
--- scripts/csi_test/entrypoint-csi-sanity.sh
+++ scripts/csi_test/entrypoint-csi-sanity.sh
@@ -5,6 +5,9 @@ sed -i -e "s/MGMT_ADDRESS/${MGMT_ADDRESS}/g" $2
 sed -i -e "s/USERNAME/${USERNAME}/g" $2
 sed -i -e "s/PASSWORD/${PASSWORD}/g" $2
 
+#update params file
+sed -i -e "s/POOL_NAME/${POOL_NAME}/g" $3
+
 /usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity  --csi.endpoint $1 --csi.secrets $2 --csi.testvolumeparameters $3  --csi.junitfile $4 --ginkgo.focus ${TESTS_TO_RUN}
 
 
