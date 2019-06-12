76f6093b39923581265c2d2cd71e748537f36ba7
olgashtivelman <olgasht@il.ibm.com>
2019-06-12 13:49:39 +0300

    CSI-101: add tests to run param

--- scripts/ci/build_and_run_sanity_test_client.sh
+++ scripts/ci/build_and_run_sanity_test_client.sh
@@ -3,6 +3,6 @@
 #docker build  -f Dockerfile-csi-test -t csi-sanity-test . && docker run  --rm  --name $1 csi-sanity-test
 
 
-echo "@@@###@@@ ${MGMT_ADDRESS} ${USERNAME} ${PASSWORD} ${TEST_TO_RUN} "
+echo "@@@###@@@ ${MGMT_ADDRESS} ${USERNAME} ${PASSWORD} ${TESTS_TO_RUN} "
 
-docker build --build-arg MGMT_ADDRESS=${MGMT_ADDRESS} --build-arg USERNAME=${USERNAME} --build-arg PASSWORD=${PASSWORD} --build-arg TEST_TO_RUN=${TEST_TO_RUN}  -f Dockerfile-csi-test -t csi-sanity-test . && docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -v$2:/tmp/test_results:rw --rm  --name $1 csi-sanity-test
+docker build --build-arg MGMT_ADDRESS=${MGMT_ADDRESS} --build-arg USERNAME=${USERNAME} --build-arg PASSWORD=${PASSWORD} --build-arg TESTS_TO_RUN=${TESTS_TO_RUN}  -f Dockerfile-csi-test -t csi-sanity-test . && docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -v$2:/tmp/test_results:rw --rm  --name $1 csi-sanity-test
