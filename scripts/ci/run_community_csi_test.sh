48a4b43c1ee9fd1624faa7977d90188bdb4997c7
olgashtivelman <olgasht@il.ibm.com>
2019-06-17 09:09:13 +0300

    CSI-101: code review fixes

---
+++ scripts/ci/run_community_csi_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xe
+
+./scripts/ci/run_controller_server_for_csi_test.sh csi-controller > csi_controller_logs 2>&1 &
+sleep 2
+mkdir -p build/reports && chmod 777 build/reports
+./scripts/ci/run_csi_test_client.sh csi-sanity-test `pwd`/build/reports/
