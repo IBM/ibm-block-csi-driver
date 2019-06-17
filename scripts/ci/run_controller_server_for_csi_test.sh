6ba4462c16afa479139541581f5d6e4ccfcaa74f
olgashtivelman <olgasht@il.ibm.com>
2019-06-17 08:58:30 +0300

    CSI-101: code review fixes

--- scripts/ci/run_controller_server_for_csi_test.sh
+++ scripts/ci/run_controller_server_for_csi_test.sh
@@ -9,5 +9,5 @@ chmod 777 /tmp/k8s_dir
 
 [ $# -ne 2 ] && { echo "Usage $0 : container_name folder_for_junit  ; exit 1; }
 
-docker build  -f Dockerfile-csi-controller -t csi-controller . &&  docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -d --rm --name $1  csi-controller -e unix://tmp/k8s_dir/f
+docker build  --no-cache -f Dockerfile-csi-controller -t csi-controller . &&  docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -d --rm --name $1  csi-controller -e unix://tmp/k8s_dir/f
 
