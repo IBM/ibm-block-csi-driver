5dee469a9d6c997079929ceb79da658a6812f231
olgashtivelman <olgasht@il.ibm.com>
2019-06-24 11:38:12 +0300

    Feature/csi 101 add csi test to ci (#21)
    Added dockerfile + jenkins pipeline for running CSI test in conatiner against csi_controller in a container in jenkins.
    there are several new files added:
    
    Dockerfile for csi test that gets the git repository of csi_test and creats all the env variables needed to run the csi test entrypoint.
    csi test entry point script that changes all the parms in the secret + parametrs file according to params recieved from the jenkins job.
    pipeline script that runs in jenkins and envokes 2 scripts that start the 2 containers.
    the idea is that the csi_contorller runs in the background waiting for the csi_test to start.
    the csi test stars with the parameters and secret and communicaates with csi_controller container via a folder /tmp/k8s_dir (there the controller creates unix socket : /tmp/k8s_dir/f )

---
+++ scripts/ci/run_community_csi_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+
+echo "${CONTROLLER_LOGS}"
+./scripts/ci/run_controller_server_for_csi_test.sh csi-controller > ${CONTROLLER_LOGS} 2>&1
+echo `pwd`
+sleep 2
+mkdir -p build/reports && chmod 777 build/reports
+./scripts/ci/run_csi_test_client.sh csi-sanity-test `pwd`/build/reports/
---
+++ scripts/csi_test/entrypoint-csi-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "JUNIT OUTPUT"
+echo ${JUNIT_OUTPUT} ${SECRET_FILE} ${PARAM_FILE} ${ENDPOINT} ${TESTS_TO_RUN_FILE}
+
+# update CSI secret
+sed -i -e "s/MGMT_ADDRESS/${MGMT_ADDRESS}/g" ${SECRET_FILE}
+sed -i -e "s/USERNAME/${USERNAME}/g" ${SECRET_FILE}
+sed -i -e "s/PASSWORD/${PASSWORD}/g" ${SECRET_FILE}
+
+# update params file
+sed -i -e "s/POOL_NAME/${POOL_NAME}/g" ${PARAM_FILE}
+
+# get tests to run
+TESTS=`cat ${TESTS_TO_RUN_FILE}| tr '\n' "|"`
+
+/usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity  --csi.endpoint ${ENDPOINT} --csi.secrets ${SECRET_FILE} --csi.testvolumeparameters ${PARAM_FILE}  --csi.junitfile ${JUNIT_OUTPUT} --ginkgo.focus ${TESTS}
