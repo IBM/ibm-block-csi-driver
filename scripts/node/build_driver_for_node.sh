18bce2f49075093d7af0a1144f7b0bec7479d653
Shay Berman <shaybery@gmail.com>
2019-06-24 15:28:09 +0300

    No need build node script - moving to makefile

--- scripts/node/build_driver_for_node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-scripts=$(dirname $0)
-
-echo "Building csi node driver"
-go build -ldflags '-w -linkmode external -extldflags "-static"' -o  $scripts/../../bin/ibm-block-csi-node-driver $scripts/../../node/cmd/main.go
-
