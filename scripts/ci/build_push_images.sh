968e5770492f4ae7f2c1f4838be0d92f9975c930
Shay Berman <shaybery@gmail.com>
2019-07-01 18:36:15 +0300

    add build csi into the scripts/ci/build_push_images.sh

--- scripts/ci/build_push_images.sh
+++ scripts/ci/build_push_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 # Validations
-MANDATORY_ENVS="IMAGE_VERSION BUILD_NUMBER DOCKER_REGISTRY CSI_CONTROLLER_IMAGE GIT_BRANCH"
+MANDATORY_ENVS="IMAGE_VERSION BUILD_NUMBER DOCKER_REGISTRY CSI_NODE_IMAGE CSI_CONTROLLER_IMAGE GIT_BRANCH"
 for envi in $MANDATORY_ENVS; do 
     [ -z "${!envi}" ] && { echo "Error - Env $envi is mandatory for the script."; exit 1; } || :
 done
@@ -12,6 +12,9 @@ specific_tag="${IMAGE_VERSION}_b${BUILD_NUMBER}_${branch}"
 
 # Set latest tag only if its from develop branch or master and prepare tags
 [ "$GIT_BRANCH" = "develop" -o "$GIT_BRANCH" = "origin/develop" -o "$GIT_BRANCH" = "master" ] && tag_latest="true" || tag_latest="false"
+
+
+# CSI controller
 ctl_registry="${DOCKER_REGISTRY}/${CSI_CONTROLLER_IMAGE}"
 ctl_tag_specific="${ctl_registry}:${specific_tag}"
 ctl_tag_latest=${ctl_registry}:latest
@@ -22,12 +25,25 @@ docker build -t ${ctl_tag_specific} $taglatestflag -f Dockerfile-csi-controller 
 docker push ${ctl_tag_specific}
 [ "$tag_latest" = "true" ] && docker push ${ctl_tag_latest} || :
 
+# CSI node
+node_registry="${DOCKER_REGISTRY}/${CSI_NODE_IMAGE}"
+node_tag_specific="${node_registry}:${specific_tag}"
+node_tag_latest=${node_registry}:latest
+[ "$tag_latest" = "true" ] && taglatestflag="-t ${node_tag_latest}" 
+
+echo "Build and push the CSI node image"
+docker build -t ${node_tag_specific} $taglatestflag -f Dockerfile-csi-controller .
+docker push ${node_tag_specific}
+[ "$tag_latest" = "true" ] && docker push ${node_tag_latest} || :
+
+
 set +x
 echo ""
 echo "Image ready:"
 echo "   ${ctl_tag_specific}"
-[ "$tag_latest" = "true" ] && echo "   ${ctl_tag_latest}" || :
+echo "   ${node_tag_specific}"
+[ "$tag_latest" = "true" ] && { echo "   ${ctl_tag_latest}"; echo "   ${node_tag_latest}"; } || :
 
 # if param $1 given the script echo the specific tag
-[ -n "$1" ] && echo ${ctl_tag_specific} > $1 || :
+[ -n "$1" ] && printf "${ctl_tag_specific}\n${node_tag_specific}\n" > $1 || :
 
