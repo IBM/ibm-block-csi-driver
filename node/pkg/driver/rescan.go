a183f90cfbf35c385792032dc5d3cd8797569b46
olgashtivelman <olgasht@il.ibm.com>
2019-08-01 14:06:23 +0300

    CSI-220: fixing multipath -f

--- node/pkg/driver/rescan.go
+++ node/pkg/driver/rescan.go
@@ -197,13 +197,15 @@ func (r RescanUtilsIscsi) FlushMultipathDevice(mpathDevice string) error {
 	_, err := r.executor.ExecuteWithTimeout(10*1000, "multipath", []string{"-f", "/dev/" + mpathDevice})
 	if err != nil {
 		if err.Error() == context.DeadlineExceeded.Error() {
-			_, err = r.executor.ExecuteWithTimeout(10*1000, "multipath", []string{"-f", "/dev/" + mpathDevice})
-			klog.V(4).Infof("string {%v} contains : {%v}. res : %v",err.Error(), "is not a valid argument", strings.Contains(  err.Error(),  "is not a valid argument"))
-			if strings.Contains(err.Error(), "is not a valid argument") {
-				klog.V(4).Infof("device was removed ")
-			} else {
-				klog.Errorf("error while running multipath command : {%v}", err.Error())
-				return err
+			out, err = r.executor.ExecuteWithTimeout(10*1000, "multipath", []string{"-f", "/dev/" + mpathDevice})
+			if err != nil {
+				klog.V(4).Infof("string {%v} contains : {%v}. res : %v", out, "is not a valid argument", strings.Contains(out, "is not a valid argument"))
+				if strings.Contains(out, "is not a valid argument") {
+					klog.V(4).Infof("device was removed ")
+				} else {
+					klog.Errorf("error while running multipath command : {%v}", err.Error())
+					return err
+				}
 			}
 		} else {
 			klog.Errorf("error while running multipath command : {%v}", err.Error())
