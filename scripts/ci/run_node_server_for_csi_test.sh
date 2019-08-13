9053a24261f686d9cf08d1d05e6c879e3fe44605
Shay Berman <shaybery@gmail.com>
2019-08-13 15:52:11 +0300

    add flag

--- scripts/ci/run_node_server_for_csi_test.sh
+++ scripts/ci/run_node_server_for_csi_test.sh
@@ -9,5 +9,5 @@ chmod 777 /tmp/k8s_dir
 
 [ $# -ne 1 ] && { echo "Usage $0 : container_name"  ; exit 1; }
 
-docker build -f Dockerfile-csi-node -t csi-node . &&  docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw -d --rm --name $1  csi-node -csi-endpoint unix://tmp/k8s_dir/nodecsi --hostname=`hostname`
+docker build -f Dockerfile-csi-node -t csi-node . &&  docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw -d --rm --name $1  csi-node -csi-endpoint unix://tmp/k8s_dir/nodecsi --hostname=`hostname` --config-file-path=./config.yaml
 
