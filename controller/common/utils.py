66f040917045e2bf3f02a0e3d96c509c857ac045
igorgon <igorgon@il.ibm.com>
2019-09-09 17:56:39 +0300

    CSI-446_igorg_get_host_by_host_identifiers

--- controller/common/utils.py
+++ controller/common/utils.py
@@ -9,8 +9,6 @@ def string_to_array(str_val, separator):
     if not str_val:
         return []
     str_val = str_val.strip()
-    if len(str_val) == 0:
-        return []
     res = str_val.split(separator)
     res[:] = [res_val.strip() for res_val in res]
     return res
