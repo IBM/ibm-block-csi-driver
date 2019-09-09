60fa81243b92d60b9c62bca8041170234ffcc154
igorgon <igorgon@il.ibm.com>
2019-09-09 18:55:04 +0300

    CSI-446_igorg_get_host_by_host_identifiers

--- controller/common/node_info.py
+++ controller/common/node_info.py
@@ -3,6 +3,10 @@ import controller.controller_server.config as config
 
 class NodeIdInfo:
     def __init__(self, node_id):
+        """
+        Args:
+            node_id: <node_name>,<iqn>,<wwns>
+        """
         node_name, iscsi_iqn, fc_wwns_str = utils.get_node_id_info(node_id)
         fc_wwns = fc_wwns_str.split(config.PARAMETERS_FC_WWN_DELIMITER)
         self.node_name = node_name
@@ -10,7 +14,15 @@ class NodeIdInfo:
 
 
 class Initiators:
+    """
+    Object containing node initiators (e.g. iqn, fc_wwns)
+    """
     def __init__(self, iscsi_iqn, fc_wwns):
+        """
+        Args:
+            iscsi_iqn : iqn
+            fc_wwns : list of fc wwns
+        """
         self.iscsi_iqn = iscsi_iqn
         self.fc_wwns = fc_wwns
         self.fc_wwns_set = set([wwn.lower() for wwn in fc_wwns])
