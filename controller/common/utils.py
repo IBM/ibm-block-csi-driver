08d92d96c1b14d52998495194e824f91dff3e081
igorgon <igorgon@il.ibm.com>
2019-09-10 16:40:28 +0300

    CSI-448_controller_logger_add_vol_id

--- controller/common/utils.py
+++ controller/common/utils.py
@@ -1,6 +1,12 @@
 import threading
 
 def set_current_thread_name(name):
+    """
+    Sets current thread name if ame not None or empty string
+    
+    Args:
+        name : name to set
+    """
     if name:
         current_thread = threading.current_thread()
         current_thread.setName(name)
