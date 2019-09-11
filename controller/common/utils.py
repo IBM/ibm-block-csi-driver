81a684da428191f38e6786794c27cab9ccc607dd
igorgonibm <53560908+igorgonibm@users.noreply.github.com>
2019-09-11 14:49:19 +0300

    feature/A9000 FC support (#65)
    CSI-446 - add sup;port for FC in XIV

---
+++ controller/common/utils.py
@@ -0,0 +1,14 @@
+def string_to_array(str_val, separator):
+    """
+    Args
+        str_val : string value
+        separator : string separator
+    Return
+        List as splitted string by separator after stripping whitespaces from each element
+    """
+    if not str_val:
+        return []
+    str_val = str_val.strip()
+    res = str_val.split(separator)
+    return [res_val.strip() for res_val in res]
+
--- controller/controller_server/csi_controller_server.py
+++ controller/controller_server/csi_controller_server.py
@@ -16,6 +16,7 @@ import controller.controller_server.utils as utils
 import controller.array_action.errors as controller_errors
 from controller.controller_server.errors import ValidationException
 import controller.controller_server.messages as messages
+from controller.common.node_info import NodeIdInfo
 
 logger = None #is set in ControllerServicer::__init__
 
@@ -182,8 +183,9 @@ class ControllerServicer(csi_pb2_grpc.ControllerServicer):
 
             array_type, vol_id = utils.get_volume_id_info(request.volume_id)
 
-            node_name, iscsi_iqn, fc_wwns_str = utils.get_node_id_info(request.node_id)
-            fc_wwns = fc_wwns_str.split(config.PARAMETERS_FC_WWN_DELIMITER)
+            node_id_info = NodeIdInfo(request.node_id)
+            node_name = node_id_info.node_name
+            initiators = node_id_info.initiators
 
             logger.debug("node name for this publish operation is : {0}".format(node_name))
 
@@ -191,7 +193,7 @@ class ControllerServicer(csi_pb2_grpc.ControllerServicer):
 
             with ArrayConnectionManager(user, password, array_addresses, array_type) as array_mediator:
 
-                host_name, connectivity_types = array_mediator.get_host_by_host_identifiers(iscsi_iqn, fc_wwns)
+                host_name, connectivity_types = array_mediator.get_host_by_host_identifiers(initiators)
 
                 logger.debug("hostname : {}, connectiivity_types  : {}".format(host_name, connectivity_types))
 
@@ -282,15 +284,16 @@ class ControllerServicer(csi_pb2_grpc.ControllerServicer):
 
             array_type, vol_id = utils.get_volume_id_info(request.volume_id)
 
-            node_name, iscsi_iqn, fc_wwns_str = utils.get_node_id_info(request.node_id)
-            fc_wwns = fc_wwns_str.split(config.PARAMETERS_FC_WWN_DELIMITER)
+            node_id_info = NodeIdInfo(request.node_id)
+            node_name = node_id_info.node_name
+            initiators = node_id_info.initiators
             logger.debug("node name for this unpublish operation is : {0}".format(node_name))
 
             user, password, array_addresses = utils.get_array_connection_info_from_secret(request.secrets)
 
             with ArrayConnectionManager(user, password, array_addresses, array_type) as array_mediator:
 
-                host_name, _ = array_mediator.get_host_by_host_identifiers(iscsi_iqn, fc_wwns)
+                host_name, _ = array_mediator.get_host_by_host_identifiers(initiators)
                 try:
                     array_mediator.unmap_volume(vol_id, host_name)
 
