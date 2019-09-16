29596dff4fedf957c81e21aff533e21204782cfd
Ran Harel <ranharel@il.ibm.com>
2019-09-16 15:46:49 +0300

    CSI-499 - get skips from environment variable in CI

--- scripts/run_csi_code_scan.sh
+++ scripts/run_csi_code_scan.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -x
 
-export SKIPS="B703"
+if [ -z "BANDIT_SKIPS" ]
+then
+    export BANDIT_SKIPS=""
+fi
+
 export OUTPUT_PATH="`pwd`/build/reports"
 
 docker build -f Dockerfile-csi-controller-code-scan -t csi-controller-code-scan . && \
-docker run --rm -t -v ${OUTPUT_PATH}:/results -e SKIPS=${SKIPS} csi-controller-code-scan
+docker run --rm -t -v ${OUTPUT_PATH}:/results -e SKIPS=${BANDIT_SKIPS} csi-controller-code-scan
 
