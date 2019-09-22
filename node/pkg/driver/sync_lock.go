0b712d8edb90bbcc581612ed7b02a9529ee2aa74
igorgon <igorgon@il.ibm.com>
2019-09-22 12:44:33 +0300

    CSI-505_node_panic_when_create_pod_with_100_volumes - remove duplicate vol id and goid from log messages

--- node/pkg/driver/sync_lock.go
+++ node/pkg/driver/sync_lock.go
@@ -48,22 +48,20 @@ func (s SyncLock) GetSyncMap() *sync.Map {
 }
 
 func (s SyncLock) AddVolumeLock(id string, msg string) error {
-	goid := util.GetGoID()
-	logger.Debugf("Lock for action %s, Try to acquire lock for volume ID=%s goid=%d", msg, id, goid)
+	logger.Debugf("Lock for action %s, Try to acquire lock for volume", msg)
 	_, exists := s.SyncMap.Load(id)
 	if !exists {
 		s.SyncMap.Store(id, 0)
-		logger.Debugf("Lock for action %s, Succeed to acquire lock for volume ID=%s goid=%d", msg, id, goid)
+		logger.Debugf("Lock for action %s, Succeed to acquire lock for volume", msg)
 		return nil
 	} else {
-		logger.Debugf("Lock for action %s, Lock for volume ID=%s is already in use by other thread. goid=%d", msg, id, goid)
+		logger.Debugf("Lock for action %s, Lock for volume is already in use by other thread", msg)
 		return &VolumeAlreadyProcessingError{id}
 	}
 }
 
 func (s SyncLock) RemoveVolumeLock(id string, msg string) {
-	goid := util.GetGoID()
-	logger.Debugf("Lock for action %s, release lock for volume ID=%s goid=%d", msg, id, goid)
+	logger.Debugf("Lock for action %s, release lock for volume", msg)
 
 	_, exists := s.SyncMap.Load(id)
 	if exists {
