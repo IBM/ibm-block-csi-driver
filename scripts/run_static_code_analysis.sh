8d419d46d7d29890234e1e537cd30ec5c8665451
oriyarde <oriyarde@ibm.com>
2020-01-08 19:14:12 +0200

    fix parens

--- scripts/run_static_code_analysis.sh
+++ scripts/run_static_code_analysis.sh
@@ -5,4 +5,4 @@ docker build -f Dockerfile-csi-controller.test -t csi-controller-tests . || exit
 EXIT_STATUS=0
 docker run --entrypoint ./controller/scripts/pycodestyle.sh --rm csi-controller-tests || EXIT_STATUS=$?
 docker run --entrypoint ./controller/scripts/pylint.sh --rm csi-controller-tests || EXIT_STATUS=$?
-exit $(EXIT_STATUS)
+exit ${EXIT_STATUS}
