de26b4d6c807c4b5dffb1d3ca0841bc6aae33824
igorgon <igorgon@il.ibm.com>
2020-02-26 18:14:06 +0200

    CSI-1191_iscsi_login_deployment

--- node/scripts/entrypoint.sh
+++ node/scripts/entrypoint.sh
@@ -1,10 +1,4 @@
 #! /bin/bash -x
 
-echo "Starting iscsid..."
-#iscsid -f &
-
-# sleep one second to make sure iscsid is running in background
-sleep 1
-
 echo "Starting driver with arguments $@..."
 /root/ibm-block-csi-node-driver $@
