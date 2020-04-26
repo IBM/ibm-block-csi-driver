f60790f3fe865158b650acac23305c0e8843c84c
ArbelNathan <arbel.nathan@ibm.com>
2020-04-26 20:51:57 +0300

    /bin/ try to run uname

--- scripts/run_yamlcheck.sh
+++ scripts/run_yamlcheck.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
-arch=$('uname -m')
-echo env.arch
+
+arch=$('/bin/uname -m')
+echo arch
 if [ "$arch" = "x86_64" ];
 then
 docker run -t -v `pwd`/deploy/kubernetes/v1.13:/deploy/kubernetes/v1.13 garethr/kubeval deploy/kubernetes/v1.13/*.yaml
