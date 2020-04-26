63f2324e76576f945b277fa99eb708fc605ba5d6
ArbelNathan <arbel.nathan@ibm.com>
2020-04-26 23:21:16 +0300

    CR fixes

--- scripts/run_yamlcheck.sh
+++ scripts/run_yamlcheck.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -xe
 
 arch=$(uname -m)
-echo arch
 if [ "$arch" = "x86_64" ];
 then
 docker run -t -v `pwd`/deploy/kubernetes/v1.13:/deploy/kubernetes/v1.13 garethr/kubeval deploy/kubernetes/v1.13/*.yaml
 docker run -t -v `pwd`/deploy/kubernetes/v1.14:/deploy/kubernetes/v1.14 garethr/kubeval deploy/kubernetes/v1.14/*.yaml
+else
+echo "Skipping yaml validations stage. garethr/kubeval only supports x86"
 fi
