4a13177dc3b1bf58b6cc092ca9ecb7f127681db2
ArbelNathan <arbel.nathan@ibm.com>
2020-04-26 20:39:24 +0300

    clean pipeline move if to bash file

--- scripts/run_yamlcheck.sh
+++ scripts/run_yamlcheck.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -xe
+arch=$('uname -m')
+echo env.arch
+if [ "$arch" = "x86_64" ];
+then
 docker run -t -v `pwd`/deploy/kubernetes/v1.13:/deploy/kubernetes/v1.13 garethr/kubeval deploy/kubernetes/v1.13/*.yaml
 docker run -t -v `pwd`/deploy/kubernetes/v1.14:/deploy/kubernetes/v1.14 garethr/kubeval deploy/kubernetes/v1.14/*.yaml
-
+fi
