e58cd4c18e75e631d2b1c4a3a1f1f2a79e78fcba
igorgon <igorgon@il.ibm.com>
2020-04-26 19:15:51 +0300

    CSI-750_snap_deployment_1_17

--- controller/tests/controller_server/csi_controller_server_test.py
+++ controller/tests/controller_server/csi_controller_server_test.py
@@ -205,7 +205,7 @@ class TestControllerServerCreateSnapshot(AbstractControllerTest):
     def test_create_snapshot_with_same_volume_name_exists_exception(self):
         self.create_snapshot_returns_error(return_code=grpc.StatusCode.ALREADY_EXISTS,
                                            err=array_errors.SnapshotNameBelongsToVolumeError("snap",
-                                                                                                     "endpoint"))
+                                                                                             "endpoint"))
 
     def test_create_snapshot_with_other_exception(self):
         self.create_snapshot_returns_error(return_code=grpc.StatusCode.INTERNAL, err=Exception("error"))
