015f18e6f2b845d1ff9330580c0008e8d9c95937
ArbelNathan <arbel.nathan@ibm.com>
2020-04-27 13:20:06 +0300

    CR fixes

--- scripts/run_yamlcheck.sh
+++ scripts/run_yamlcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 arch=$(uname -m)
-if [ "$arch" = "x86_64" ];
+if [[ "$arch" == "x86"* ]];
 then
 docker run -t -v `pwd`/deploy/kubernetes/v1.13:/deploy/kubernetes/v1.13 garethr/kubeval deploy/kubernetes/v1.13/*.yaml
 docker run -t -v `pwd`/deploy/kubernetes/v1.14:/deploy/kubernetes/v1.14 garethr/kubeval deploy/kubernetes/v1.14/*.yaml
