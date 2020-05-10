d8d261104c0a54407705111c98ff9e1db9ca835d
igorgon <igorgon@il.ibm.com>
2020-05-10 12:58:21 +0300

    CSI-753_create_vol_from_snapshot

--- controller/controller_server/csi_controller_server.py
+++ controller/controller_server/csi_controller_server.py
@@ -123,7 +123,7 @@ class ControllerServicer(csi_pb2_grpc.ControllerServicer):
                         context.set_details("Volume is not empty.")
                         context.set_code(grpc.StatusCode.INTERNAL
                         return csi_pb2.CreateVolumeResponse()
-                    
+
                     logger.error("Copy Snapshot {0} data to Volume {1}.".format(src_snapshot_id, vol_name))
                     array_mediator.copy_volume_from_snapshot(vol_name, src_snapshot_id)
                     vol.copy_src_object_id = src_snapshot_id
