a6cc3c8fea4e935f48a1c70f99295d9607185c53
igorgon <igorgon@il.ibm.com>
2020-05-14 15:36:16 +0300

    CSI-753_create_vol_from_snapshot

--- controller/array_action/messages.py
+++ controller/array_action/messages.py
@@ -59,4 +59,4 @@ SnapshotNameBelongsToVolumeError_message = "Snapshot not found. Volume with the 
 SnapshotIdBelongsToVolumeError_message = "Snapshot not found. Volume with the same id exists. \
                                                          Id : {0} , array : {1}"
 
-SnapshotWrongVolume_message = "Snapshot {0} exists but it is of Volume {1} and not {2}"
+SnapshotWrongVolumeError_message = "Snapshot {0} exists but it is of Volume {1} and not {2}"
