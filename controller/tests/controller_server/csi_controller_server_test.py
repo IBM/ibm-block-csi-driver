a665536aaa9da661e6f4f2deb4e7d0ad808c1947
igorgon <igorgon@il.ibm.com>
2020-05-26 17:49:59 +0300

    CSI-1263_connection_pool

--- controller/tests/controller_server/csi_controller_server_test.py
+++ controller/tests/controller_server/csi_controller_server_test.py
@@ -60,9 +60,9 @@ class TestControllerServerCreateVolume(unittest.TestCase):
         agent = Mock()
         agent.get_mediator = Mock()
         agent.get_mediator.return_value = Mock()
-        agent.get_mediator.__enter__ = Mock()
-        agent.get_mediator.__enter__.return_value = self.mediator
-        agent.__enter__ = Mock()
+        # agent.get_mediator.__enter__ = Mock()
+        # agent.get_mediator.__enter__.return_value = self.mediator
+        # agent.__enter__ = Mock()
         agent.__enter__.return_value = self.mediator
         get_agent.return_value = agent
 
