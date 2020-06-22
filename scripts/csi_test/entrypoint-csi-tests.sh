19a39df5c7ad4c74ba988d044ac7d9875ff774df
oriyarde <57443811+oriyarde@users.noreply.github.com>
2020-06-22 17:20:02 +0300

    Add/csi 1528 snapshots community tests (#189)
    add create delete snapshot community tests

--- scripts/csi_test/entrypoint-csi-tests.sh
+++ scripts/csi_test/entrypoint-csi-tests.sh
@@ -1,22 +1,20 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-echo "JUNIT OUTPUT"
-echo ${JUNIT_OUTPUT} ${SECRET_FILE} ${PARAM_FILE} ${ENDPOINT} ${TESTS_TO_RUN_FILE}
-
-
-echo "TESTT ${STORAGE_ARRAYS}  ${USERNAME}  ${PASSWORD}"
-
-# update CSI secret
+echo "update CSI secret"
 sed -i -e "s/STORAGE_ARRAYS/${STORAGE_ARRAYS}/g" ${SECRET_FILE}
 sed -i -e "s/USERNAME/${USERNAME}/g" ${SECRET_FILE}
 sed -i -e "s/PASSWORD/${PASSWORD}/g" ${SECRET_FILE}
 
-echo "TESTT ${STORAGE_ARRAYS}  ${USERNAME}  ${PASSWORD}"
-
-# update params file
+echo "update params file"
 sed -i -e "s/POOL_NAME/${POOL_NAME}/g" ${PARAM_FILE}
 
 # get tests to run
-TESTS=`cat ${TESTS_TO_RUN_FILE}| tr '\n' "|"`
+TESTS=`cat ${TESTS_TO_RUN_FILE}| sed -Ez '$ s/\n+$//' | tr '\n' "|"`
 
-/usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity  --csi.endpoint ${ENDPOINT} --csi.controllerendpoint ${ENDPOINT_CONTROLLER} --csi.secrets ${SECRET_FILE} --csi.testvolumeparameters ${PARAM_FILE}  --csi.junitfile ${JUNIT_OUTPUT} --ginkgo.focus ${TESTS}
+/usr/local/go/src/github.com/kubernetes-csi/csi-test/cmd/csi-sanity/csi-sanity \
+--csi.endpoint ${ENDPOINT} \
+--csi.controllerendpoint ${ENDPOINT_CONTROLLER} \
+--csi.secrets ${SECRET_FILE} \
+--csi.testvolumeparameters ${PARAM_FILE} \
+--csi.junitfile ${JUNIT_OUTPUT} \
+--ginkgo.focus ${TESTS}
