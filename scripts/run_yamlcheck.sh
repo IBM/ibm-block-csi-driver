b518bfdf820477e0ef750fbe37a6fb47195f59d9
Anna Lisyuk <anna.lisyuk@ibm.com>
2020-09-08 10:45:09 +0300

    Update yamlcheck script

--- scripts/run_yamlcheck.sh
+++ scripts/run_yamlcheck.sh
@@ -3,8 +3,7 @@
 arch=$(uname -m)
 if [[ "$arch" == "x86"* ]];
 then
-docker run -t -v `pwd`/deploy/kubernetes/v1.13:/deploy/kubernetes/v1.13 garethr/kubeval deploy/kubernetes/v1.13/*.yaml
-docker run -t -v `pwd`/deploy/kubernetes/v1.14:/deploy/kubernetes/v1.14 garethr/kubeval deploy/kubernetes/v1.14/*.yaml
+docker run -t -v `pwd`/deploy/kubernetes/examples:/deploy/kubernetes/examples garethr/kubeval deploy/kubernetes/examples/*.yaml
 else
 echo "Skipping yaml validations stage. garethr/kubeval only supports x86"
 fi
