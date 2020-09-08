69111ca8d3bf09c84ec324ffee37d53ab7d5904d
Anna Lisyuk <anna.lisyuk@ibm.com>
2020-09-08 11:29:53 +0300

    Add ignore missing schemas to yamlcheck script

--- scripts/run_yamlcheck.sh
+++ scripts/run_yamlcheck.sh
@@ -3,7 +3,7 @@
 arch=$(uname -m)
 if [[ "$arch" == "x86"* ]];
 then
-docker run -t -v `pwd`/deploy/kubernetes/examples:/deploy/kubernetes/examples garethr/kubeval deploy/kubernetes/examples/*.yaml
+docker run -t -v `pwd`/deploy/kubernetes/examples:/deploy/kubernetes/examples garethr/kubeval --ignore-missing-schemas deploy/kubernetes/examples/*.yaml
 else
 echo "Skipping yaml validations stage. garethr/kubeval only supports x86"
 fi
