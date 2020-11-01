d6666828f3a37268ac3287ae7a883ab5e2b197c1
matancarmeli7 <45543087+matancarmeli7@users.noreply.github.com>
2020-11-01 17:06:18 +0200

    Update run_unitests.sh

--- scripts/run_unitests.sh
+++ scripts/run_unitests.sh
@@ -2,4 +2,4 @@
 
 [ -n "$1" ] && coverage="-v $1:/driver/coverage:z"
 docker build -f Dockerfile-csi-controller.test -t csi-controller-tests . && \
-docker run --entrypoint ./controller/scripts/unitests.sh --rm -t $coverage csi-controller-tests
+docker run -e CRYPTOGRAPHY_ALLOW_OPENSSL_102=true --entrypoint ./controller/scripts/unitests.sh --rm -t $coverage csi-controller-tests
