c9e20b6482ea32ea201cee612deb725ec94b3c15
oriyarde <oriyarde@ibm.com>
2020-11-22 17:30:53 +0200

    fill community test node log

--- scripts/ci/run_controller_server_for_csi_test.sh
+++ scripts/ci/run_controller_server_for_csi_test.sh
@@ -10,5 +10,5 @@ chmod 777 /tmp/k8s_dir
 [ $# -ne 1 ] && { echo "Usage $0 : container_name"  ; exit 1; }
 
 docker build -f Dockerfile-csi-controller -t csi-controller . &&  \
-docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw -d --network host --rm --name $1  csi-controller -e unix://tmp/k8s_dir/f
+docker run -v /tmp/k8s_dir:/tmp/k8s_dir:rw -d --network host --name $1  csi-controller -e unix://tmp/k8s_dir/f
 
