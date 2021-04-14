e5cc1c143dad40728d4d2fea5d98cff4a86aa534
zingero <zinger777@gmail.com>
2021-04-14 11:20:19 +0300

    Printing the exit codes of the scripts we run when we test

--- controller/scripts/entrypoint-test.sh
+++ controller/scripts/entrypoint-test.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -x
+
 ./controller/scripts/pycodestyle.sh
+PYCODESTYLE=$?
+
 ./controller/scripts/pylint.sh
+PYLINT=$?
+
 ./controller/scripts/unitests.sh
+UNITESTS=$?
+
+set +x
+echo "-------- Summary of exit codes --------"
+echo "pycodestyle: $PYCODESTYLE"
+echo "pylint: $PYLINT"
+echo "unitests: $UNITESTS"
