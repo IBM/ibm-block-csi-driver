bdbc8fab0d5c54690eceabd1e6614a1c597ebe80
Barak Davidov <barakda@il.ibm.com>
2021-04-25 21:21:30 +0300

    issue fix

--- scripts/ci/run_csi_test_client.sh
+++ scripts/ci/run_csi_test_client.sh
@@ -3,7 +3,7 @@
 [ $# -ne 2 ] && { echo "Usage $0 : container_name folder_for_junit"  ; exit 1; }
 
 #/tmp/k8s_dir is the directory of the csi grpc\unix socket that shared between csi server and csi-test docker 
-if [ $1 = 'community_svc' ] ; then
+if [ $3 = 'community_svc' ] ; then
     docker build -f Dockerfile-csi-svc-test -t csi-sanity-test .  && docker run --user=root -e STORAGE_ARRAYS=${STORAGE_ARRAYS} -e USERNAME=${USERNAME} -e PASSWORD=${PASSWORD}  -e POOL_NAME=${POOL_NAME} -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -v$2:/tmp/test_results:rw --rm  --name $1 csi-sanity-test
 then
     docker build -f Dockerfile-csi-test -t csi-sanity-test .  && docker run --user=root -e STORAGE_ARRAYS=${STORAGE_ARRAYS} -e USERNAME=${USERNAME} -e PASSWORD=${PASSWORD}  -e POOL_NAME=${POOL_NAME} -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -v$2:/tmp/test_results:rw --rm  --name $1 csi-sanity-test
