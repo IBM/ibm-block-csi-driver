faeac6d90a5cf3099d1923c00c260446c3f4c837
Barak Davidov <barakda@il.ibm.com>
2021-04-29 10:13:23 +0300

    passing csi_params as arg

--- scripts/ci/run_csi_test_client.sh
+++ scripts/ci/run_csi_test_client.sh
@@ -2,9 +2,11 @@
 
 [ $# -ne 3 ] && { echo "Usage $0 : container_name folder_for_junit"  ; exit 1; }
 
-#/tmp/k8s_dir is the directory of the csi grpc\unix socket that shared between csi server and csi-test docker 
+#/tmp/k8s_dir is the directory of the csi grpc\unix socket that shared between csi server and csi-test docker
 if [ $3 = 'community_svc' ] ; then
-    docker build -f Dockerfile-csi-svc-test -t csi-sanity-test .  && docker run --user=root -e STORAGE_ARRAYS=${STORAGE_ARRAYS} -e USERNAME=${USERNAME} -e PASSWORD=${PASSWORD}  -e POOL_NAME=${POOL_NAME} -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -v$2:/tmp/test_results:rw --rm  --name $1 csi-sanity-test
+    csi_params='csi_params_thin'
 else
-    docker build -f Dockerfile-csi-test -t csi-sanity-test .  && docker run --user=root -e STORAGE_ARRAYS=${STORAGE_ARRAYS} -e USERNAME=${USERNAME} -e PASSWORD=${PASSWORD}  -e POOL_NAME=${POOL_NAME} -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -v$2:/tmp/test_results:rw --rm  --name $1 csi-sanity-test
+    csi_params='csi_params'
 fi
+docker build -f Dockerfile-csi-test --build-arg CSI_PARAMS=${csi_params} -t csi-sanity-test .  && docker run --user=root -e STORAGE_ARRAYS=${STORAGE_ARRAYS} -e USERNAME=${USERNAME} -e PASSWORD=${PASSWORD}  -e POOL_NAME=${POOL_NAME} -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -v$2:/tmp/test_results:rw --rm  --name $1 csi-sanity-test
+
