e0272d9bb804c616d7333b22141f0ff04caa3f5d
zingero <zinger777@gmail.com>
2021-05-23 16:04:43 +0300

    CSI-2906 enabling global statement linting (#326)
    * CSI-2906 enabling global statement linting
    
    * CSI-2778 initializing the logger before running main function

--- controller/common/csi_logger.py
+++ controller/common/csi_logger.py
@@ -1,19 +1,21 @@
 import logging
 import sys
 
-log_level = "DEBUG"
-ENTRY = '%(asctime)s %(levelname)s\t[%(thread)d] [%(threadName)s] (%(filename)s:%(funcName)s:%(lineno)d) - %(message)s'
+logger_properties = {
+    'log_level': 'DEBUG',
+    'entry': '%(asctime)s %(levelname)s\t[%(thread)d] [%(threadName)s] '
+             '(%(filename)s:%(funcName)s:%(lineno)d) - %(message)s'
+}
 
 
 def get_stdout_logger():
     csi_logger = logging.getLogger("csi_logger")
 
     if not getattr(csi_logger, 'handler_set', None):
-        global log_level
-        csi_logger.setLevel(log_level)
+        csi_logger.setLevel(logger_properties['log_level'])
         handler = logging.StreamHandler(sys.stdout)
         handler.setLevel(logging.DEBUG)
-        formatter = logging.Formatter(ENTRY)
+        formatter = logging.Formatter(logger_properties['entry'])
         handler.setFormatter(formatter)
         csi_logger.addHandler(handler)
 
@@ -27,6 +29,7 @@ def set_log_level(log_level_to_set):
     In order to set non-default log level this function should be called before first cal of get_stdout_logger
     :param log_level_to_set:
     """
-    global log_level
     if log_level_to_set:
-        log_level = log_level_to_set.upper()
+        logger_properties['log_level'] = log_level_to_set.upper()
+        csi_logger = logging.getLogger("csi_logger")
+        csi_logger.setLevel(logger_properties['log_level'])
--- controller/controller_server/csi_controller_server.py
+++ controller/controller_server/csi_controller_server.py
@@ -13,7 +13,7 @@ import controller.controller_server.utils as utils
 from controller.array_action import messages
 from controller.array_action.storage_agent import get_agent, detect_array_type
 from controller.common import settings
-from controller.common.csi_logger import get_stdout_logger, set_log_level, logging
+from controller.common.csi_logger import get_stdout_logger, set_log_level
 from controller.common.node_info import NodeIdInfo
 from controller.common.utils import set_current_thread_name
 from controller.controller_server.errors import ObjectIdError, ValidationException
@@ -21,8 +21,7 @@ from controller.controller_server.exception_handler import handle_common_excepti
 from controller.csi_general import csi_pb2
 from controller.csi_general import csi_pb2_grpc
 
-logger = None  # is set in ControllerServicer::__init__
-logger: logging.Logger
+logger = get_stdout_logger()
 
 
 class ControllerServicer(csi_pb2_grpc.ControllerServicer):
@@ -31,9 +30,6 @@ class ControllerServicer(csi_pb2_grpc.ControllerServicer):
     """
 
     def __init__(self, array_endpoint):
-        # init logger
-        global logger
-        logger = get_stdout_logger()
 
         self.endpoint = array_endpoint
 
@@ -743,13 +739,9 @@ def main():
     parser.add_argument("-l", "--loglevel", dest="loglevel", help="log level")
     arguments = parser.parse_args()
 
-    # set logger level and init logger
-    log_level = arguments.loglevel
-    set_log_level(log_level)
+    set_log_level(arguments.loglevel)
 
-    # start the server
-    endpoint = arguments.endpoint
-    controller_servicer = ControllerServicer(endpoint)
+    controller_servicer = ControllerServicer(arguments.endpoint)
     controller_servicer.start_server()
 
 
