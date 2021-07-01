1fe56d83645277296338a5b82eb1a805080e166c
matancarmeli7 <matan.carmeli7@gmail.com>
2021-07-01 11:00:23 +0300

    add environment variables to yq command

--- scripts/ci/setup_ci_dependencies.sh
+++ scripts/ci/setup_ci_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 set +o pipefail
 
-echo $'yq() {\n  docker run --rm -e operator_image_for_test=$operator_image_for_test -i -v "${PWD}":/workdir mikefarah/yq:4 "$@"\n}' >> /home/runner/.bash_profile
+echo $'yq() {\n  docker run --rm -e operator_image_for_test=$operator_image_for_test -e controller_repository_for_test=$controller_repository_for_test -e node_repository_for_test=$node_repository_for_test -e driver_images_tag=$driver_images_tag -i -v "${PWD}":/workdir mikefarah/yq:4 "$@"\n}' >> /home/runner/.bash_profile
