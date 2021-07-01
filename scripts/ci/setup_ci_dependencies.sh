0eb3674f4ca5eeb283744073ead1779bacccce09
matancarmeli7 <matan.carmeli7@gmail.com>
2021-07-01 16:58:26 +0300

    add docker-hub command

--- scripts/ci/setup_ci_dependencies.sh
+++ scripts/ci/setup_ci_dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -xe
 set +o pipefail
 
+python -m pip install --upgrade pip docker-hub==2.2.0
+echo yq > dev-requirements.txt
+
 echo $'yq() {\n  docker run --rm -e operator_image_for_test=$operator_image_for_test -e controller_repository_for_test=$controller_repository_for_test -e node_repository_for_test=$node_repository_for_test -e driver_images_tag=$driver_images_tag -i -v "${PWD}":/workdir mikefarah/yq:4 "$@"\n}' >> /home/runner/.bash_profile
