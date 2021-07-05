34f9715387f418846f49ba9952af35f9b19e8ec7
matancarmeli7 <matan.carmeli7@gmail.com>
2021-07-05 11:49:31 +0300

    made yq butifule

--- scripts/ci/setup_ci_dependencies.sh
+++ scripts/ci/setup_ci_dependencies.sh
@@ -2,6 +2,14 @@
 set +o pipefail
 
 python -m pip install --upgrade pip docker-hub==2.2.0
-echo yq > dev-requirements.txt
+echo docker-hub > dev-requirements.txt
 
-echo $'yq() {\n  docker run --rm -e operator_image_for_test=$operator_image_for_test -e controller_repository_for_test=$controller_repository_for_test -e node_repository_for_test=$node_repository_for_test -e driver_images_tag=$driver_images_tag -i -v "${PWD}":/workdir mikefarah/yq:4 "$@"\n}' >> /home/runner/.bash_profile
+cat >>/tmp/myconfig.conf <<'EOL'
+yq() {
+  docker run --rm -e operator_image_for_test=$operator_image_for_test\
+                  -e controller_repository_for_test=$controller_repository_for_test\
+                  -e node_repository_for_test=$node_repository_for_test\
+                  -e driver_images_tag=$driver_images_tag\
+                  -i -v "${PWD}":/workdir mikefarah/yq "$@"
+}
+EOL
