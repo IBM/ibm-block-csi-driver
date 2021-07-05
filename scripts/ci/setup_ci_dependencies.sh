057884fbf55b2a8f982aef28740c142243b24b1f
matancarmeli7 <matan.carmeli7@gmail.com>
2021-07-05 13:25:17 +0300

    change the .bash_profile path

--- scripts/ci/setup_ci_dependencies.sh
+++ scripts/ci/setup_ci_dependencies.sh
@@ -4,7 +4,7 @@ set +o pipefail
 python -m pip install --upgrade pip docker-hub==2.2.0
 echo docker-hub > dev-requirements.txt
 
-cat >>/tmp/myconfig.conf <<'EOL'
+cat >>/home/runner/.bash_profile <<'EOL'
 yq() {
   docker run --rm -e operator_image_for_test=$operator_image_for_test\
                   -e controller_repository_for_test=$controller_repository_for_test\
