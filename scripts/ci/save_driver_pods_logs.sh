ed661c54bd6016e50389faef3c7d5aa859ad3c85
matancarmeli7 <matan.carmeli7@gmail.com>
2021-07-12 17:22:46 +0300

    fix save logs

--- scripts/ci/save_driver_pods_logs.sh
+++ scripts/ci/save_driver_pods_logs.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -x
 
+get_all_pods_by_type (){
+    pod_name=$1
+    kubectl get pod -l csi | grep $pod_name | awk '{print$1}'
+}
+
 run_action_on_pod (){
     pod_name=$1
     action=$2
     extra_args=$3
-    get_all_pods_by_type="kubectl get pod -l csi | grep $pod_name | awk '{print$1}'"
-    kubectl $action $($get_all_pods_by_type) $extra_args -c ibm-block-csi-$pod_name > /tmp/driver_$($get_all_pods_by_type)_$action.txt
+    kubectl $action $(get_all_pods_by_type $pod_name) $extra_args > "/tmp/driver_$(get_all_pods_by_type $pod_name)_${action}.txt"
+
 }
 
 declare -a pod_types=(
@@ -17,5 +22,5 @@ declare -a pod_types=(
 for pod_type in "${pod_types[@]}"
 do
     run_action_on_pod $pod_type logs "-c ibm-block-csi-$pod_name"
-    run_action_on_pod $pod_type describe ""
+    run_action_on_pod $pod_type "describe pod" ""
 done
