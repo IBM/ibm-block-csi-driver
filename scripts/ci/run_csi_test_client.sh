56b2a3a60b2f2e596503a10d83c04d38f332c7cf
Tal Yichye <tal.yichye@ibm.com>
2021-07-22 10:33:54 +0300

    add mapping for csi-mount folder

--- scripts/ci/run_csi_test_client.sh
+++ scripts/ci/run_csi_test_client.sh
@@ -8,5 +8,5 @@ else
     csi_params='csi_params'
 fi
 #/tmp/k8s_dir is the directory of the csi grpc\unix socket that shared between csi server and csi-test docker
-docker build -f Dockerfile-csi-test --build-arg CSI_PARAMS=${csi_params} -t csi-sanity-test .  && docker run --user=root -e STORAGE_ARRAYS=${STORAGE_ARRAYS} -e USERNAME=${USERNAME} -e PASSWORD=${PASSWORD}  -e POOL_NAME=${POOL_NAME} -v /tmp/k8s_dir:/tmp/k8s_dir:rw  -v$2:/tmp/test_results:rw --rm  --name $1 csi-sanity-test
+docker build -f Dockerfile-csi-test --build-arg CSI_PARAMS=${csi_params} -t csi-sanity-test .  && docker run --user=root -e STORAGE_ARRAYS=${STORAGE_ARRAYS} -e USERNAME=${USERNAME} -e PASSWORD=${PASSWORD}  -e POOL_NAME=${POOL_NAME} -v /tmp/k8s_dir:/tmp/k8s_dir:rw -v /tmp/csi-mount:/tmp/csi-mount:rw -v$2:/tmp/test_results:rw --rm  --name $1 csi-sanity-test
 
