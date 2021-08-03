0477825f2488fa0d22fe7cd2576b59a0cd47af86
matancarmeli7 <matan.carmeli7@gmail.com>
2021-08-03 11:57:08 +0300

    change stages names

--- scripts/ci/github_actions/driver/setup_ci_dependencies.sh
+++ scripts/ci/github_actions/driver/setup_ci_dependencies.sh
@@ -1,15 +1,26 @@
 #!/bin/bash -xe
 set +o pipefail
 
-python -m pip install --upgrade pip
-echo docker-hub==2.2.0 > dev-requirements.txt
-pip install -r dev-requirements.txt
+install_ci_dependencies (){
+  python -m pip install --upgrade pip
+  echo docker-hub==2.2.0 > dev-requirements.txt
+  pip install -r dev-requirements.txt
+}
 
-source /home/runner/.bash_profile
-cd common
-image_version=`yq eval .identity.version config.yaml`
-cd -
+get_driver_version (){
+  source /home/runner/.bash_profile
+  cd common
+  image_version=`yq eval .identity.version config.yaml`
+  cd -
+}
 
+install_ci_dependencies
+cat >>/home/runner/.bash_profile <<'EOL'
+yq() {
+  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
+}
+EOL
+get_driver_version
 GITHUB_SHA=${GITHUB_SHA:0:7}_
 driver_image_tags=`scripts/ci/get_image_tags_from_branch.sh ${CI_ACTION_REF_NAME} ${image_version} ${build_number} ${GITHUB_SHA}`
 docker_image_branch_tag=`echo $driver_image_tags | awk '{print$2}'`
