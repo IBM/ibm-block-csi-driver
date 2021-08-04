3496a4aab2517a4cad963d4b2651d7c34570c513
matancarmeli7 <matan.carmeli7@gmail.com>
2021-08-04 09:49:39 +0300

    change commit sha parameter

--- scripts/ci/get_image_tags_from_branch.sh
+++ scripts/ci/get_image_tags_from_branch.sh
@@ -2,8 +2,8 @@
 GIT_BRANCH=$1
 IMAGE_VERSION=$2
 BUILD_NUMBER=$3
-commit_sha=$4
+COMMIT_HASH=$4
 branch_tag=`echo $GIT_BRANCH| sed 's|/|.|g'`  #not sure if docker accept / in the version
-specific_tag="${IMAGE_VERSION}_b${BUILD_NUMBER}_${commit_sha}${branch_tag}"
+specific_tag="${IMAGE_VERSION}_b${BUILD_NUMBER}_${COMMIT_HASH}${branch_tag}"
 echo $specific_tag
 echo $branch_tag
