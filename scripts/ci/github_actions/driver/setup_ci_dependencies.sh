8ff6f1cae6fc2c326bc1b3d7af10b5a1b82d6fa1
matancarmeli7 <matan.carmeli7@gmail.com>
2021-10-03 14:58:38 +0300

    fix setup_ci_dependencies script comments
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

--- scripts/ci/github_actions/driver/setup_ci_dependencies.sh
+++ scripts/ci/github_actions/driver/setup_ci_dependencies.sh
@@ -5,24 +5,22 @@ install_ci_dependencies (){
   python -m pip install --upgrade pip
   echo docker-hub==2.2.0 > dev-requirements.txt
   pip install -r dev-requirements.txt
-}
-
-get_driver_version (){
-  source /home/runner/.bash_profile
-  cd common
-  image_version=`yq eval .identity.version config.yaml`
-  cd -
-}
-
-install_ci_dependencies
 cat >>/home/runner/.bash_profile <<'EOL'
 yq() {
   docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
 }
 EOL
-get_driver_version
+}
+
+get_driver_version (){
+  source /home/runner/.bash_profile
+  yq eval .identity.version common/config.yaml
+}
+
+install_ci_dependencies
+driver_version=$(get_driver_version)
 GITHUB_SHA=${GITHUB_SHA:0:7}_
-driver_image_tags=`scripts/ci/get_image_tags_from_branch.sh ${CI_ACTION_REF_NAME} ${image_version} ${build_number} ${GITHUB_SHA}`
+driver_image_tags=`scripts/ci/get_image_tags_from_branch.sh ${CI_ACTION_REF_NAME} ${driver_version} ${build_number} ${GITHUB_SHA}`
 docker_image_branch_tag=`echo $driver_image_tags | awk '{print$2}'`
 driver_images_specific_tag=`echo $driver_image_tags | awk '{print$1}'`
 
