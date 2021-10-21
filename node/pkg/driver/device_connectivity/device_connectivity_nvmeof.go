ac4214de7998f133ad83e9b5257f123f7dc1fb89
arbelnat <arbel.nathan@ibm.com>
2021-10-21 16:53:25 +0300

    added nvme file

---
+++ node/pkg/driver/device_connectivity/device_connectivity_nvmeof.go
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2019 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package device_connectivity
+
+import (
+	"github.com/ibm/ibm-block-csi-driver/node/pkg/driver/executer"
+)
+
+type OsDeviceConnectivityNvmeOFc struct {
+	Executer          executer.ExecuterInterface
+	HelperScsiGeneric OsDeviceConnectivityHelperScsiGenericInterface
+}
+
+func NewOsDeviceConnectivityNvmeOFc(executer executer.ExecuterInterface) OsDeviceConnectivityInterface {
+	return &OsDeviceConnectivityNvmeOFc{
+		Executer:          executer,
+		HelperScsiGeneric: NewOsDeviceConnectivityHelperScsiGeneric(executer),
+	}
+}
+
+func (r OsDeviceConnectivityNvmeOFc) EnsureLogin(_ map[string][]string) {
+	// Nvme doesn't require login
+}
+
+func (r OsDeviceConnectivityNvmeOFc) RescanDevices(_ int, _ []string) error {
+	return nil
+}
+
+func (r OsDeviceConnectivityNvmeOFc) GetMpathDevice(volumeId string) (string, error) {
+	/*
+	   Return Value: "dm-X" of the volumeID.
+	*/
+	return r.HelperScsiGeneric.GetMpathDevice(volumeId)
+}
+
+func (r OsDeviceConnectivityNvmeOFc) FlushMultipathDevice(mpathDevice string) error {
+	return r.HelperScsiGeneric.FlushMultipathDevice(mpathDevice)
+}
+
+func (r OsDeviceConnectivityNvmeOFc) RemovePhysicalDevice(sysDevices []string) error {
+	return r.HelperScsiGeneric.RemovePhysicalDevice(sysDevices)
+}
