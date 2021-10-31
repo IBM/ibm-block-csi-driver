cda5430d55a24618c61bc6df7645244e80ec2e5b
matancarmeli7 <matan.carmeli7@gmail.com>
2021-10-31 16:56:44 +0200

    PR
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

--- scripts/ci/build_push_images.sh
+++ scripts/ci/build_push_images.sh
@@ -7,7 +7,7 @@ for envi in $MANDATORY_ENVS; do
 done
 
 # Prepare specific tag for the image
-tags=`scripts/ci/get_image_tags_from_branch.sh ${GIT_BRANCH} ${IMAGE_VERSION} ${BUILD_NUMBER}`
+tags=`scripts/ci/get_image_tags_from_branch.sh ${GIT_BRANCH} ${IMAGE_VERSION} ${BUILD_NUMBER} ${GIT_COMMIT}`
 specific_tag=`echo $tags | awk '{print$1}'`
 
 # Set latest tag only if its from develop branch or master and prepare tags
--- scripts/ci/github_actions/driver/setup_ci_dependencies.sh
+++ scripts/ci/github_actions/driver/setup_ci_dependencies.sh
@@ -3,10 +3,10 @@ set +o pipefail
 
 install_ci_dependencies (){
   scripts/ci/github_actions/setup_yq.sh
+  source /home/runner/.bash_profile
   python -m pip install --upgrade pip==21.2.4
   echo docker-hub==2.2.0 > dev-requirements.txt
   pip install -r dev-requirements.txt
-  source /home/runner/.bash_profile
 }
 
 get_driver_version (){
@@ -15,14 +15,13 @@ get_driver_version (){
 
 install_ci_dependencies
 driver_version=$(get_driver_version)
-GITHUB_SHA=${GITHUB_SHA:0:7}_
-driver_image_tags=`scripts/ci/get_image_tags_from_branch.sh ${CI_ACTION_REF_NAME} ${driver_version} ${build_number} ${GITHUB_SHA}`
-docker_image_branch_tag=`echo $driver_image_tags | awk '{print$2}'`
-driver_images_specific_tag=`echo $driver_image_tags | awk '{print$1}'`
+driver_image_tags=$(scripts/ci/get_image_tags_from_branch.sh ${CI_ACTION_REF_NAME} ${driver_version} ${build_number} ${GITHUB_SHA})
+driver_images_specific_tag=$(echo $driver_image_tags | awk '{print$1}')
+docker_image_branch_tag=$(echo $driver_image_tags | awk '{print$2}')
 
 if [ "$docker_image_branch_tag" == "develop" ]; then
   docker_image_branch_tag=latest
 fi
 
-echo "::set-output name=docker_image_branch_tag::${docker_image_branch_tag}"
 echo "::set-output name=driver_images_specific_tag::${driver_images_specific_tag}"
+echo "::set-output name=docker_image_branch_tag::${docker_image_branch_tag}"
