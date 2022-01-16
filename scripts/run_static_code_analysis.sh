b7eaa2fb9d141c0d513079cf324a3134957948a7
arbelnat <arbel.nathan@ibm.com>
2022-01-16 13:08:33 +0200

    try to trigger csi_pb2 build

--- scripts/run_static_code_analysis.sh
+++ scripts/run_static_code_analysis.sh
@@ -4,7 +4,6 @@ set -x
 docker build -f Dockerfile-csi-controller.test -t csi-controller-tests . || exit 1
 
 EXIT_STATUS=0
-docker run --entrypoint ./controller/scripts/csi_pb2.sh || EXIT_STATUS=$?
 docker run --entrypoint ./controller/scripts/pycodestyle.sh --rm csi-controller-tests || EXIT_STATUS=$?
 docker run --entrypoint ./controller/scripts/pylint.sh --rm csi-controller-tests || EXIT_STATUS=$?
 exit ${EXIT_STATUS}
