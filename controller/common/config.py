84fe05d72a4eaf0c7412848cb8707e8f643af247
arbelnat <arbel.nathan@ibm.com>
2022-04-25 15:39:39 +0300

    PR

--- controller/common/config.py
+++ controller/common/config.py
@@ -3,9 +3,9 @@ import os.path
 import yaml
 from munch import DefaultMunch
 
-_my_path = os.path.abspath(os.path.dirname(__file__))
-_path = os.path.join(_my_path, "../../common/config.yaml")
+_this_file_path = os.path.abspath(os.path.dirname(__file__))
+_config_path = os.path.join(_this_file_path, "../../common/config.yaml")
 
-with open(_path, 'r') as yamlfile:
+with open(_config_path, 'r') as yamlfile:
     _cfg = yaml.safe_load(yamlfile)  # TODO: add the following when possible : Loader=yaml.FullLoader)
 config = DefaultMunch.fromDict(_cfg)
