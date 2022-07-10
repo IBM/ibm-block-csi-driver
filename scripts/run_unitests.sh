1fc83b8e63dd537b2847965a139d77609fab941d
Matan Carmeli <45543087+matancarmeli7@users.noreply.github.com>
2022-07-10 14:28:41 +0300

    Revert "Feature/csi 4164 host definer kubernetes controller (#514)"
    This reverts commit f647933887c0d7df6779104b98b848a89ad891b3.

--- scripts/run_unitests.sh
+++ scripts/run_unitests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 [ -n "$1" ] && coverage="-v $1:/driver/coverage:z"
-docker build -f Dockerfile-controllers.test -t csi-controller-tests . && \
+docker build -f Dockerfile-csi-controller.test -t csi-controller-tests . && \
 docker run --entrypoint ./controllers/scripts/unitests.sh --rm -t $coverage csi-controller-tests
