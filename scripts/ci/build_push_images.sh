62bfe4bac855afd888d2bd447eb62c11e5eb6eab
matancarmeli7 <matan.carmeli7@gmail.com>
2022-08-15 11:25:56 +0300

    handle save images name
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

--- scripts/ci/build_push_images.sh
+++ scripts/ci/build_push_images.sh
@@ -12,6 +12,7 @@ specific_tag="${IMAGE_VERSION}_b${BUILD_NUMBER}_${branch}"
 
 # Set latest tag only if its from develop branch or master and prepare tags
 [ "$GIT_BRANCH" = "develop" -o "$GIT_BRANCH" = "origin/develop" -o "$GIT_BRANCH" = "master" ] && tag_latest="true" || tag_latest="false"
+[ -n "$1" ] && printf "" > $1 || :
 
 build_and_push (){
     repository=$1
@@ -26,6 +27,10 @@ build_and_push (){
     docker build -t $tag_specific $taglatestflag -f $dockerfile --build-arg VERSION="${IMAGE_VERSION}" --build-arg BUILD_NUMBER="${BUILD_NUMBER}" .
     docker push $tag_specific
     [ "$tag_latest" = "true" ] && docker push $tag_latest || :
+    [ -n "$1" ] && printf "${tag_specific}\n" >> $1 || :
+    echo ""
+    echo "Image ready:"
+    echo "   ${tag_specific}"
 }
 
 # CSI controller
@@ -39,15 +44,3 @@ build_and_push $CSI_NODE_IMAGE Dockerfile-csi-node node
 # Host Definer
 # --------
 build_and_push $CSI_HOST_DEFINER_IMAGE Dockerfile-csi-host-definer "Host Definer"
-
-set +x
-echo ""
-echo "Image ready:"
-echo "   ${ctl_tag_specific}"
-echo "   ${node_tag_specific}"
-echo "   ${host_definer_tag_specific}"
-[ "$tag_latest" = "true" ] && { echo "   ${ctl_tag_latest}"; echo "   ${node_tag_latest}"; echo "   ${host_definer_tag_latest}";} || :
-
-# if param $1 given the script echo the specific tag
-[ -n "$1" ] && printf "${ctl_tag_specific}\n${node_tag_specific}\n${host_definer_tag_specific}\n" > $1 || :
-
