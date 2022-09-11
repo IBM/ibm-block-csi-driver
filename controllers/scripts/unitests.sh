525f0da79d0955d6d112549b4a014087a7133a88
ArbelNathan <arbel.nathan@ibm.com>
2022-09-11 23:49:27 +0300

    take abstract mediator test out of controller's and mock mediator
    Signed-off-by: ArbelNathan <arbel.nathan@ibm.com>

--- controllers/scripts/unitests.sh
+++ controllers/scripts/unitests.sh
@@ -2,4 +2,5 @@
 set -x
 coveragedir=/driver/coverage/
 [ ! -d $coveragedir ] && mkdir -p $coveragedir
-exec nosetests --exe --with-coverage --cover-xml --cover-xml-file=$coveragedir/.coverage.xml --cover-package=common --cover-package=controllers --with-xunit --xunit-file=$coveragedir/.unitests.xml $@
+#exec nosetests --exe --with-coverage --cover-xml --cover-xml-file=$coveragedir/.coverage.xml --cover-package=common --cover-package=controllers --with-xunit --xunit-file=$coveragedir/.unitests.xml $@
+pytest --cov-report term-missing --cov=controllers controllers/
