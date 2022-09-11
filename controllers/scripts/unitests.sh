d61e7afd34a24c1f2d00409631948d1cca04de14
ArbelNathan <arbel.nathan@ibm.com>
2022-09-11 23:50:39 +0300

    revert changes
    Signed-off-by: ArbelNathan <arbel.nathan@ibm.com>

--- controllers/scripts/unitests.sh
+++ controllers/scripts/unitests.sh
@@ -2,5 +2,4 @@
 set -x
 coveragedir=/driver/coverage/
 [ ! -d $coveragedir ] && mkdir -p $coveragedir
-#exec nosetests --exe --with-coverage --cover-xml --cover-xml-file=$coveragedir/.coverage.xml --cover-package=common --cover-package=controllers --with-xunit --xunit-file=$coveragedir/.unitests.xml $@
-pytest --cov-report term-missing --cov=controllers controllers/
+exec nosetests --exe --with-coverage --cover-xml --cover-xml-file=$coveragedir/.coverage.xml --cover-package=common --cover-package=controllers --with-xunit --xunit-file=$coveragedir/.unitests.xml $@
