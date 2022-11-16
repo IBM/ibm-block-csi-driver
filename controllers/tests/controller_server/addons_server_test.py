6213d0a4386b22cdfbee3ebf8113d141be7691b1
Elena Gershkovich <elenage@il.ibm.com>
2022-11-16 10:25:38 +0200

    Separate classes for unit tests
    Signed-off-by: Elena Gershkovich <elenage@il.ibm.com>

--- controllers/tests/controller_server/addons_server_test.py
+++ controllers/tests/controller_server/addons_server_test.py
@@ -19,14 +19,7 @@ from controllers.tests.utils import ProtoBufMock
 ADDON_SERVER_PATH = "controllers.servers.csi.addons_server"
 
 
-class TestControllerServicerEnableVolumeReplication(unittest.TestCase, CommonControllerTest):
-    @property
-    def tested_method(self):
-        return self.servicer.EnableVolumeReplication
-
-    @property
-    def tested_method_response_class(self):
-        return pb2.EnableVolumeReplicationResponse
+class BaseControllerServicerSetUp(unittest.TestCase):
 
     def setUp(self):
         self.servicer = ReplicationControllerServicer()
@@ -41,9 +34,6 @@ class TestControllerServicerEnableVolumeReplication(unittest.TestCase, CommonCon
                                 "management_address": SECRET_MANAGEMENT_ADDRESS_VALUE}
         self.request.volume_id = "{}:{};{}".format("A9000", OBJECT_INTERNAL_ID, VOLUME_UID)
         self.request.replication_id = "{}:{};{}".format("A9000", OTHER_OBJECT_INTERNAL_ID, VOLUME_UID)
-        self.mediator.get_object_by_id = Mock()
-        self.mediator.get_object_by_id.return_value = utils.get_mock_mediator_response_volume(10, VOLUME_NAME,
-                                                                                              VOLUME_UID, "xiv")
         self.context = utils.FakeContext()
 
     def _prepare_replication_mocks(self, replication_type=None, copy_type=COPY_TYPE, is_primary=False):
@@ -67,6 +57,22 @@ class TestControllerServicerEnableVolumeReplication(unittest.TestCase, CommonCon
             replication_request = ReplicationRequest(OBJECT_INTERNAL_ID, None, None, REPLICATION_COPY_TYPE_SYNC,
                                                      REPLICATION_TYPE_EAR, replication_name)
         return replication_request
+
+
+class TestControllerServicerEnableVolumeReplication(BaseControllerServicerSetUp, CommonControllerTest):
+    @property
+    def tested_method(self):
+        return self.servicer.EnableVolumeReplication
+
+    @property
+    def tested_method_response_class(self):
+        return pb2.EnableVolumeReplicationResponse
+
+    def setUp(self):
+        super().setUp()
+        self.mediator.get_object_by_id = Mock()
+        self.mediator.get_object_by_id.return_value = utils.get_mock_mediator_response_volume(10, VOLUME_NAME,
+                                                                                              VOLUME_UID, "xiv")
 
     def _test_enable_replication_succeeds(self, replication_type):
         replication_request = self._prepare_request_params(replication_type)
@@ -98,74 +104,6 @@ class TestControllerServicerEnableVolumeReplication(unittest.TestCase, CommonCon
         self.mediator.get_replication.assert_called_once_with(replication_request)
         self.mediator.create_replication.assert_not_called()
 
-    def _test_disable_replication_succeeds(self, replication_type):
-        replication_request = self._prepare_request_params(replication_type)
-        self._prepare_replication_mocks(replication_type=replication_type)
-
-        self.servicer.DisableVolumeReplication(self.request, self.context)
-
-        self.assertEqual(grpc.StatusCode.OK, self.context.code)
-        replication = utils.get_mock_mediator_response_replication(name=REPLICATION_NAME,
-                                                                   replication_type=replication_type)
-        self.mediator.get_replication.assert_called_once_with(replication_request)
-        self.mediator.delete_replication.assert_called_once_with(replication)
-
-    def _test_disable_replication_idempotency_succeeds(self, replication_type):
-        replication_request = self._prepare_request_params(replication_type)
-        self._prepare_replication_mocks()
-
-        self.servicer.DisableVolumeReplication(self.request, self.context)
-
-        self.assertEqual(grpc.StatusCode.OK, self.context.code)
-        self.mediator.get_replication.assert_called_once_with(replication_request)
-        self.mediator.delete_replication.assert_not_called()
-
-    def _test_promote_replication_succeeds(self, replication_type, is_primary=False):
-        replication_request = self._prepare_request_params(replication_type)
-        self._prepare_replication_mocks(replication_type=replication_type, is_primary=is_primary)
-
-        self.servicer.PromoteVolume(self.request, self.context)
-
-        self.assertEqual(grpc.StatusCode.OK, self.context.code)
-        self.mediator.get_replication.assert_called_once_with(replication_request)
-
-    def _test_promote_replication_fails(self, replication_type, grpc_status=grpc.StatusCode.FAILED_PRECONDITION):
-        replication_request = self._prepare_request_params(replication_type)
-        self._prepare_replication_mocks()
-
-        self.servicer.PromoteVolume(self.request, self.context)
-
-        self.assertEqual(grpc_status, self.context.code)
-        self.mediator.get_replication.assert_called_once_with(replication_request)
-        self.mediator.promote_replication_volume.assert_not_called()
-
-    def _test_demote_replication_succeeds(self, replication_type, is_primary=False):
-        replication_request = self._prepare_request_params(replication_type)
-        self._prepare_replication_mocks(replication_type=replication_type, is_primary=is_primary)
-
-        self.servicer.DemoteVolume(self.request, self.context)
-
-        self.assertEqual(grpc.StatusCode.OK, self.context.code)
-        self.mediator.get_replication.assert_called_once_with(replication_request)
-
-    def _test_promote_replication_idempotency_succeeds(self, replication_type):
-        replication_request = self._prepare_request_params(replication_type)
-        self._prepare_replication_mocks(replication_type=replication_type, is_primary=True)
-
-        self.servicer.PromoteVolume(self.request, self.context)
-
-        self.assertEqual(grpc.StatusCode.OK, self.context.code)
-        self.mediator.get_replication.assert_called_once_with(replication_request)
-        self.mediator.promote_replication_volume.assert_not_called()
-
-    def _test_resync_replication(self, replication_type, grpc_status=grpc.StatusCode.OK):
-        replication_request = self._prepare_request_params(replication_type)
-
-        self.servicer.ResyncVolume(self.request, self.context)
-
-        self.assertEqual(grpc_status, self.context.code)
-        self.mediator.get_replication.assert_called_once_with(replication_request)
-
     def test_enable_replication_succeeds(self):
         self._test_enable_replication_succeeds(REPLICATION_TYPE_MIRROR)
 
@@ -179,44 +117,6 @@ class TestControllerServicerEnableVolumeReplication(unittest.TestCase, CommonCon
 
     def test_enable_replication_no_volume_fails(self):
         self._test_enable_replication_no_volume_fails()
-
-    def test_disable_replication_succeeds(self):
-        self._test_disable_replication_succeeds(replication_type=REPLICATION_TYPE_MIRROR)
-
-    def test_disable_replication_idempotency_succeeds(self):
-        self._test_disable_replication_idempotency_succeeds(REPLICATION_TYPE_MIRROR)
-
-    def test_promote_replication_succeeds(self):
-        replication = utils.get_mock_mediator_response_replication(name=REPLICATION_NAME,
-                                                                   replication_type=REPLICATION_TYPE_MIRROR)
-        self._test_promote_replication_succeeds(REPLICATION_TYPE_MIRROR)
-        self.mediator.promote_replication_volume.assert_called_once_with(replication)
-
-    def test_promote_replication_idempotency_succeeds(self):
-        self._test_promote_replication_succeeds(REPLICATION_TYPE_MIRROR, True)
-        self.mediator.promote_replication_volume.assert_not_called()
-
-    def test_promote_replication_fails(self):
-        self._test_promote_replication_fails(REPLICATION_TYPE_MIRROR)
-
-    def test_demote_replication_succeeds(self):
-        replication = utils.get_mock_mediator_response_replication(name=REPLICATION_NAME,
-                                                                   replication_type=REPLICATION_TYPE_MIRROR,
-                                                                   is_primary=True)
-        self._test_demote_replication_succeeds(REPLICATION_TYPE_MIRROR, is_primary=True)
-        self.mediator.demote_replication_volume.assert_called_once_with(replication)
-
-    def test_demote_replication_idempotency_succeeds(self):
-        self._test_demote_replication_succeeds(REPLICATION_TYPE_MIRROR)
-        self.mediator.demote_replication_volume.assert_not_called()
-
-    def test_resync_replication_succeeds(self):
-        self._prepare_replication_mocks(replication_type=REPLICATION_TYPE_MIRROR)
-        self._test_resync_replication(REPLICATION_TYPE_MIRROR)
-
-    def test_resync_replication_fails(self):
-        self._prepare_replication_mocks()
-        self._test_resync_replication(REPLICATION_TYPE_MIRROR, grpc.StatusCode.FAILED_PRECONDITION)
 
     def test_enable_replication_already_processing(self):
         self._test_request_already_processing("volume_id", self.request.volume_id)
@@ -241,8 +141,193 @@ class TestControllerServicerEnableVolumeReplication(unittest.TestCase, CommonCon
                                                   copy_type=REPLICATION_COPY_TYPE_SYNC,
                                                   grpc_status=grpc.StatusCode.ALREADY_EXISTS)
 
+
+class TestControllerServicerDisableVolumeReplication(BaseControllerServicerSetUp, CommonControllerTest):
+    @property
+    def tested_method(self):
+        return self.servicer.DisableVolumeReplication
+
+    @property
+    def tested_method_response_class(self):
+        return pb2.DisableVolumeReplicationResponse
+
+    def _test_disable_replication_succeeds(self, replication_type):
+        replication_request = self._prepare_request_params(replication_type)
+        self._prepare_replication_mocks(replication_type=replication_type)
+
+        self.servicer.DisableVolumeReplication(self.request, self.context)
+
+        self.assertEqual(grpc.StatusCode.OK, self.context.code)
+        replication = utils.get_mock_mediator_response_replication(name=REPLICATION_NAME,
+                                                                   replication_type=replication_type)
+        self.mediator.get_replication.assert_called_once_with(replication_request)
+        self.mediator.delete_replication.assert_called_once_with(replication)
+
+    def _test_disable_replication_idempotency_succeeds(self, replication_type):
+        replication_request = self._prepare_request_params(replication_type)
+        self._prepare_replication_mocks()
+
+        self.servicer.DisableVolumeReplication(self.request, self.context)
+
+        self.assertEqual(grpc.StatusCode.OK, self.context.code)
+        self.mediator.get_replication.assert_called_once_with(replication_request)
+        self.mediator.delete_replication.assert_not_called()
+
+    def test_disable_replication_succeeds(self):
+        self._test_disable_replication_succeeds(replication_type=REPLICATION_TYPE_MIRROR)
+
+    def test_disable_replication_idempotency_succeeds(self):
+        self._test_disable_replication_idempotency_succeeds(REPLICATION_TYPE_MIRROR)
+
+    def test_disable_replication_already_processing(self):
+        self._test_request_already_processing("volume_id", self.request.volume_id)
+
+    def test_disable_replication_with_wrong_secrets(self):
+        self._test_request_with_wrong_secrets()
+
+    def test_disable_replication_with_array_connection_exception(self):
+        self._test_request_with_array_connection_exception()
+
     def test_disable_ear_replication_succeeds(self):
         self._test_disable_replication_succeeds(REPLICATION_TYPE_EAR)
 
+
+class TestControllerServicerPromoteVolumeReplication(BaseControllerServicerSetUp, CommonControllerTest):
+    @property
+    def tested_method(self):
+        return self.servicer.PromoteVolume
+
+    @property
+    def tested_method_response_class(self):
+        return pb2.PromoteVolumeResponse
+
+    def _test_promote_replication_succeeds(self, replication_type, is_primary=False):
+        replication_request = self._prepare_request_params(replication_type)
+        self._prepare_replication_mocks(replication_type=replication_type, is_primary=is_primary)
+
+        self.servicer.PromoteVolume(self.request, self.context)
+
+        self.assertEqual(grpc.StatusCode.OK, self.context.code)
+        self.mediator.get_replication.assert_called_once_with(replication_request)
+
+    def _test_promote_replication_fails(self, replication_type, grpc_status=grpc.StatusCode.FAILED_PRECONDITION):
+        replication_request = self._prepare_request_params(replication_type)
+        self._prepare_replication_mocks()
+
+        self.servicer.PromoteVolume(self.request, self.context)
+
+        self.assertEqual(grpc_status, self.context.code)
+        self.mediator.get_replication.assert_called_once_with(replication_request)
+        self.mediator.promote_replication_volume.assert_not_called()
+
+    def test_promote_replication_succeeds(self):
+        replication = utils.get_mock_mediator_response_replication(name=REPLICATION_NAME,
+                                                                   replication_type=REPLICATION_TYPE_MIRROR)
+        self._test_promote_replication_succeeds(REPLICATION_TYPE_MIRROR)
+        self.mediator.promote_replication_volume.assert_called_once_with(replication)
+
+    def test_promote_replication_idempotency_succeeds(self):
+        self._test_promote_replication_succeeds(REPLICATION_TYPE_MIRROR, True)
+        self.mediator.promote_replication_volume.assert_not_called()
+
+    def test_promote_replication_fails(self):
+        self._test_promote_replication_fails(REPLICATION_TYPE_MIRROR)
+
+    def test_promote_replication_already_processing(self):
+        self._test_request_already_processing("volume_id", self.request.volume_id)
+
+    def test_promote_replication_with_wrong_secrets(self):
+        self._test_request_with_wrong_secrets()
+
+    def test_promote_replication_with_array_connection_exception(self):
+        self._test_request_with_array_connection_exception()
+
     def test_promote_ear_replication_succeeds(self):
         self._test_promote_replication_succeeds(REPLICATION_TYPE_EAR)
+
+    def test_promote_ear_replication_fails(self):
+        self._test_promote_replication_fails(REPLICATION_TYPE_EAR)
+
+
+class TestControllerServicerDemoteVolumeReplication(BaseControllerServicerSetUp, CommonControllerTest):
+    @property
+    def tested_method(self):
+        return self.servicer.DemoteVolume
+
+    @property
+    def tested_method_response_class(self):
+        return pb2.DemoteVolumeResponse
+
+    def _test_demote_replication_succeeds(self, replication_type, is_primary=False):
+        replication_request = self._prepare_request_params(replication_type)
+        self._prepare_replication_mocks(replication_type=replication_type, is_primary=is_primary)
+
+        self.servicer.DemoteVolume(self.request, self.context)
+
+        self.assertEqual(grpc.StatusCode.OK, self.context.code)
+        self.mediator.get_replication.assert_called_once_with(replication_request)
+
+    def _test_demote_replication_fails(self, replication_type, grpc_status=grpc.StatusCode.FAILED_PRECONDITION):
+        replication_request = self._prepare_request_params(replication_type)
+        self._prepare_replication_mocks()
+
+        self.servicer.DemoteVolume(self.request, self.context)
+
+        self.assertEqual(grpc_status, self.context.code)
+        self.mediator.get_replication.assert_called_once_with(replication_request)
+        self.mediator.promote_replication_volume.assert_not_called()
+
+    def test_demote_replication_succeeds(self):
+        replication = utils.get_mock_mediator_response_replication(name=REPLICATION_NAME,
+                                                                   replication_type=REPLICATION_TYPE_MIRROR,
+                                                                   is_primary=True)
+        self._test_demote_replication_succeeds(REPLICATION_TYPE_MIRROR, is_primary=True)
+        self.mediator.demote_replication_volume.assert_called_once_with(replication)
+
+    def test_demote_replication_fails(self):
+        self._test_demote_replication_fails(REPLICATION_TYPE_MIRROR)
+
+    def test_demote_replication_idempotency_succeeds(self):
+        self._test_demote_replication_succeeds(REPLICATION_TYPE_MIRROR)
+        self.mediator.demote_replication_volume.assert_not_called()
+
+    def test_demote_replication_already_processing(self):
+        self._test_request_already_processing("volume_id", self.request.volume_id)
+
+    def test_demote_replication_with_wrong_secrets(self):
+        self._test_request_with_wrong_secrets()
+
+    def test_demote_replication_with_array_connection_exception(self):
+        self._test_request_with_array_connection_exception()
+
+    def test_demote_ear_replication_succeeds(self):
+        self._test_demote_replication_succeeds(REPLICATION_TYPE_EAR)
+
+    def test_demote_ear_replication_fails(self):
+        self._test_demote_replication_fails(REPLICATION_TYPE_EAR)
+
+
+class TestControllerServicerResyncVolumeReplication(BaseControllerServicerSetUp, CommonControllerTest):
+    @property
+    def tested_method(self):
+        return self.servicer.ResyncVolume
+
+    @property
+    def tested_method_response_class(self):
+        return pb2.ResyncVolumeResponse
+
+    def _test_resync_replication(self, replication_type, grpc_status=grpc.StatusCode.OK):
+        replication_request = self._prepare_request_params(replication_type)
+
+        self.servicer.ResyncVolume(self.request, self.context)
+
+        self.assertEqual(grpc_status, self.context.code)
+        self.mediator.get_replication.assert_called_once_with(replication_request)
+
+    def test_resync_replication_succeeds(self):
+        self._prepare_replication_mocks(replication_type=REPLICATION_TYPE_MIRROR)
+        self._test_resync_replication(REPLICATION_TYPE_MIRROR)
+
+    def test_resync_replication_fails(self):
+        self._prepare_replication_mocks()
+        self._test_resync_replication(REPLICATION_TYPE_MIRROR, grpc.StatusCode.FAILED_PRECONDITION)
