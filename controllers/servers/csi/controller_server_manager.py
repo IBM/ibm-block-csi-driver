0de475c0d3621f71499116b0da13f734e89ddc21
ArbelNathan <arbel.nathan@ibm.com>
2023-05-15 17:09:12 +0300

    add fence sidecar to controller
    Signed-off-by: ArbelNathan <arbel.nathan@ibm.com>

--- controllers/servers/csi/controller_server_manager.py
+++ controllers/servers/csi/controller_server_manager.py
@@ -3,7 +3,7 @@ import time
 from concurrent import futures
 
 import grpc
-from csi_general import csi_pb2_grpc, replication_pb2_grpc, volumegroup_pb2_grpc
+from csi_general import csi_pb2_grpc, replication_pb2_grpc, volumegroup_pb2_grpc, fence_pb2_grpc
 
 from controllers.common.config import config
 from controllers.common.csi_logger import get_stdout_logger
@@ -11,6 +11,7 @@ from controllers.common.settings import CSI_CONTROLLER_SERVER_WORKERS
 from controllers.servers.csi.addons_server import ReplicationControllerServicer
 from controllers.servers.csi.csi_controller_server import CSIControllerServicer
 from controllers.servers.csi.volume_group_server import VolumeGroupControllerServicer
+from controllers.servers.csi.fence import FenceControllerServicer
 
 logger = get_stdout_logger()
 
@@ -26,6 +27,7 @@ class ControllerServerManager:
         self.csi_servicer = CSIControllerServicer()
         self.replication_servicer = ReplicationControllerServicer()
         self.volume_group_servicer = VolumeGroupControllerServicer()
+        self.fence_servicer = FenceControllerServicer()
 
     def start_server(self):
         max_workers = get_max_workers_count()
@@ -35,6 +37,7 @@ class ControllerServerManager:
         csi_pb2_grpc.add_IdentityServicer_to_server(self.csi_servicer, controller_server)
         replication_pb2_grpc.add_ControllerServicer_to_server(self.replication_servicer, controller_server)
         volumegroup_pb2_grpc.add_ControllerServicer_to_server(self.volume_group_servicer, controller_server)
+        fence_pb2_grpc.add_FenceControllerServicer_to_server(self.fence_servicer, controller_server)
 
         # bind the server to the port defined above
         # controller_server.add_insecure_port('[::]:{}'.format(self.server_port))
