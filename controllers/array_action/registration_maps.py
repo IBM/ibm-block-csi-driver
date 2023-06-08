fd9c675f8a2f9217aca24415e672db107cf77950
Matan Carmeli <45543087+matancarmeli7@users.noreply.github.com>
2023-06-08 09:50:13 +0300

    Feature/csi 5609 basic flow call home (#677)
    * add register plugin
    
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>
    
    * add new empty line
    
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>
    
    * add unit tests
    
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>
    
    * PR
    
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>
    
    * PR
    
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>
    
    ---------
    
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

---
+++ controllers/array_action/registration_maps.py
@@ -0,0 +1,38 @@
+from controllers.array_action.settings import METADATA_KEY
+from controllers.servers.settings import UNIQUE_KEY_KEY
+
+
+def _generate_plugin_type(unique_key, metadata=''):
+    return {
+        UNIQUE_KEY_KEY: unique_key,
+        METADATA_KEY: metadata
+    }
+
+
+basic_plugin_type = _generate_plugin_type('basic')
+replication_plugin_type = _generate_plugin_type('replication')
+volume_group_plugin_type = _generate_plugin_type('volume_group')
+snapshot_plugin_type = _generate_plugin_type('snapshot')
+host_definition_plugin_type = _generate_plugin_type('host_definition')
+
+
+REGISTRATION_MAP = {
+    'create_volume': basic_plugin_type,
+    'delete_volume': basic_plugin_type,
+    'map_volume': basic_plugin_type,
+    'unmap_volume': basic_plugin_type,
+    'create_replication': replication_plugin_type,
+    'delete_replication': replication_plugin_type,
+    'promote_replication_volume': replication_plugin_type,
+    'demote_replication_volume': replication_plugin_type,
+    'create_volume_group': volume_group_plugin_type,
+    'delete_volume_group': volume_group_plugin_type,
+    'add_volume_to_volume_group': volume_group_plugin_type,
+    'remove_volume_from_volume_group': volume_group_plugin_type,
+    'create_snapshot': snapshot_plugin_type,
+    'delete_snapshot': snapshot_plugin_type,
+    'create_host': host_definition_plugin_type,
+    'delete_host': host_definition_plugin_type,
+    'add_ports_to_host': host_definition_plugin_type,
+    'remove_ports_from_host': host_definition_plugin_type,
+}
