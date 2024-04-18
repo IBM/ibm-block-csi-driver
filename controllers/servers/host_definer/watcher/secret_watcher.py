5f7f0d253f6b2bf772b6b249f7556202b6054baf
Ariel Kass <arielk@il.ibm.com>
2024-04-18 22:22:03 +0300

    missed 1 rename

--- controllers/servers/host_definer/watcher/secret_watcher.py
+++ controllers/servers/host_definer/watcher/secret_watcher.py
@@ -3,7 +3,7 @@ from kubernetes import watch
 import controllers.servers.host_definer.messages as messages
 from controllers.common.csi_logger import get_stdout_logger
 from controllers.servers.host_definer.watcher.watcher_helper import Watcher, MANAGED_SECRETS
-from controllers.servers.host_definer.types import SecretInfo
+from controllers.servers.host_definer.hd_types import SecretInfo
 from controllers.servers.host_definer import settings
 
 logger = get_stdout_logger()
