2f719e101f3de9a549821e47503d215418002284
Eyal Perek <eyalp@il.ibm.com>
2024-07-30 06:20:19 +0300

    CSI-5727: Controller - SVC lsnvmefabric execution does not depend on lsnvmefabric attribute

--- controllers/array_action/array_mediator_svc.py
+++ controllers/array_action/array_mediator_svc.py
@@ -50,6 +50,8 @@ NOT_ENOUGH_EXTENTS_IN_POOL_CREATE = 'CMMVC8710E'
 NOT_VALID_IO_GROUP = 'CMMVC5729E'
 NOT_SUPPORTED_PARAMETER = 'CMMVC5709E'
 CANNOT_CHANGE_HOST_PROTOCOL_BECAUSE_OF_MAPPED_PORTS = 'CMMVC9331E'
+COMMAND_NOT_SUPPORTED = 'CMMVC7205E'
+BASH_COMMAND_NOT_FOUND = 'command not found'
 
 HOST_NQN = 'nqn'
 HOST_WWPN = 'WWPN'
@@ -985,18 +987,18 @@ class SVCArrayMediator(ArrayMediatorAbstract, VolumeGroupInterface):
         try:
             return self.client.svcinfo.lsnvmefabric(remotenqn=host_nqn).as_list
         except(svc_errors.CommandExecutionError, CLIFailureError) as ex:
+            if BASH_COMMAND_NOT_FOUND in ex.my_message or COMMAND_NOT_SUPPORTED in ex.my_message:
+                logger.warning("Failed to get nvme fabrics - command not supported")
+                return None
             logger.error("Failed to get nvme fabrics. Reason "
                          "is: {0}".format(ex))
             raise ex
 
-    def _is_lsnvmefabric_supported(self):
-        return hasattr(self.client.svcinfo, "lsnvmefabric")
-
     def _get_host_names_by_nqn(self, nqn):
-        if self._is_lsnvmefabric_supported():
-            nvme_fabrics = self._lsnvmefabric(nqn)
-            return set(nvme_fabric.object_name for nvme_fabric in nvme_fabrics)
-        return None
+        nvme_fabrics = self._lsnvmefabric(nqn)
+        if nvme_fabrics is None:
+            return None
+        return set(nvme_fabric.object_name for nvme_fabric in nvme_fabrics)
 
     def _lshostiplogin(self, iqn):
         try:
