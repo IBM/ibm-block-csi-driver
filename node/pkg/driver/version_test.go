d0dbf78c469eb84a0dd34793c8789a108d3b3faa
Uri Ziv <uri.ziv1@ibm.com>
2024-09-12 16:51:53 +0300

    Bump version 1.11.3 to 1.11.4 (without docs at this commit)
    Signed-off-by: Uri Ziv <uri.ziv1@ibm.com>

--- node/pkg/driver/version_test.go
+++ node/pkg/driver/version_test.go
@@ -46,7 +46,7 @@ func TestGetVersion(t *testing.T) {
 	version, err := GetVersion(dir)
 
 	expected := VersionInfo{
-		DriverVersion: "1.11.3",
+		DriverVersion: "1.11.4",
 		GitCommit:     "",
 		BuildDate:     "",
 		GoVersion:     runtime.Version(),
@@ -76,7 +76,7 @@ func TestGetVersionJSON(t *testing.T) {
 	}
 
 	expected := fmt.Sprintf(`{
-  "driverVersion": "1.11.3",
+  "driverVersion": "1.11.4",
   "gitCommit": "",
   "buildDate": "",
   "goVersion": "%s",
