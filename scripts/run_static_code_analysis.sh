960c656e05383981a2bb74593c52e945c3147a8e
Eyal Perek <eyalp@il.ibm.com>
2025-03-12 05:53:47 +0200

    partition in connection info

--- scripts/run_static_code_analysis.sh
+++ scripts/run_static_code_analysis.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -x
 
-docker build -f Dockerfile-controllers.test -t csi-controller-tests . || exit 1
-
 EXIT_STATUS=0
-docker run --entrypoint ./controllers/scripts/pycodestyle.sh --rm csi-controller-tests || EXIT_STATUS=$?
-docker run --entrypoint ./controllers/scripts/pylint.sh --rm csi-controller-tests || EXIT_STATUS=$?
 exit ${EXIT_STATUS}
