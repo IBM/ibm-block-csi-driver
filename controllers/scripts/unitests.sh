717ab5942b3a8add3682721c95ad3f0a4f2fa1f4
Eyal Perek <eyalp@il.ibm.com>
2025-04-23 21:09:21 +0300

    Revert "disable UT"
    This reverts commit 1a55bd3824237a14fb1ee3539b47e37781308288.

--- controllers/scripts/unitests.sh
+++ controllers/scripts/unitests.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -x
-exit 0
+coveragedir=/driver/coverage/
+[ ! -d $coveragedir ] && mkdir -p $coveragedir
+exec nosetests --exe --with-coverage --cover-xml --cover-xml-file=$coveragedir/.coverage.xml --cover-package=common --cover-package=controllers --with-xunit --xunit-file=$coveragedir/.unitests.xml $@
--- scripts/run_unitests.sh
+++ scripts/run_unitests.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -x
-exit 0
+[ -n "$1" ] && coverage="-v $1:/driver/coverage:z"
+podman build -f Dockerfile-controllers.test -t csi-controller-tests . && \
+podman run --entrypoint ./controllers/scripts/unitests.sh --rm -t $coverage csi-controller-tests
