a0241a29824431395a4304be86cd056d43c68371
Uri Ziv <uri.ziv1@ibm.com>
2025-11-23 09:58:13 +0200

    test

--- controllers/servers/host_definer/kubernetes_manager/manager.py
+++ controllers/servers/host_definer/kubernetes_manager/manager.py
@@ -36,13 +36,17 @@ class KubernetesManager():
     def _get_crds_info(self):
         """get_crds_info"""
         logger.info("DEBUG - uriziv - 5")
-        api = client.ApiextensionsV1Api()
-        crds = api.list_custom_resource_definition()
-        for crd in crds.items:
-            logger.info("Name: %s", crd.metadata.name)
-            logger.info("Group: %s", crd.spec.group)
-            logger.info("Versions: %s", [v.name for v in crd.spec.versions])
-            logger.info("%s", "-" * 40)
+        for k8s_node in self.core_api.list_node().items:
+            logger.info("k8s_node.metadata: %s", k8s_node.metadata)
+            node_info = NodeInfo(k8s_node.metadata.name, k8s_node.metadata.labels)
+            logger.info("node_info: %s", node_info)
+        # api = client.ApiextensionsV1Api()
+        # crds = api.list_custom_resource_definition()
+        # for crd in crds.items:
+        #     logger.info("Name: %s", crd.metadata.name)
+        #     logger.info("Group: %s", crd.spec.group)
+        #     logger.info("Versions: %s", [v.name for v in crd.spec.versions])
+        #     logger.info("%s", "-" * 40)
         logger.info("DEBUG - uriziv - 6")
 
     def _get_csi_nodes_api(self):
