3aa7c5b91f5b58d633500fdd1eee93add3d13646
Uri Ziv <uri.ziv1@ibm.com>
2025-12-01 15:29:46 +0200

    fix unitest - ValueError: node id has wrong format

--- controllers/servers/host_definer/kubernetes_manager/manager.py
+++ controllers/servers/host_definer/kubernetes_manager/manager.py
@@ -51,11 +51,17 @@ class KubernetesManager():
         logger.info(k8s_node)
         if k8s_node:
             return self._generate_node_id_info(k8s_node)
-        logger.info("DEBUG - uriziv - 27")
-        return NodeIdInfo(',,,')
+        node_id_for_node_id_info_object = ',,,'
+        logger.info(node_id_for_node_id_info_object)
+        logger.info("DEBUG - uriziv - 28")
+        return NodeIdInfo(node_id_for_node_id_info_object)
 
     def _generate_node_id_info(self, k8s_node):
-        return NodeIdInfo("%s,,," % k8s_node.metadata.name)
+        logger.info("DEBUG - uriziv - 29")
+        node_id_for_node_id_info_object = f"{k8s_node.metadata.name},,,"
+        logger.info("DEBUG - uriziv - 30")
+        return NodeIdInfo(node_id_for_node_id_info_object)
+    
 
     def _get_csi_nodes_api(self):
         return self.dynamic_client.resources.get(api_version=settings.STORAGE_API_VERSION,
