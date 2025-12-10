299c57f0ab260e9ce6ce00c42d759c42b67feaa5
Eyal Ben David <eyalbe@il.ibm.com>
2025-12-10 13:01:11 +0200

    Metadata - fix RE group

--- controllers/common/utils.py
+++ controllers/common/utils.py
@@ -107,9 +107,7 @@ def _default_callhome_metadata_aux():
     #
     ocp_version = None
     if ocp_match:
-        logger.info("OCP group(0) = {}".format(ocp_match.group()))
-        logger.info("OCP groups = {}".format(ocp_match.groups()))
-        ocp_version = ocp_match.group()
+        ocp_version = ocp_match.group(1)
 
     if ocp_version:
         ch_info.append(f"ocp:{ocp_version}")
