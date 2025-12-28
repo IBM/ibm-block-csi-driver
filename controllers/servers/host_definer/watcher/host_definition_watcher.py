eaa739dfbf048d104425c29b17d293946eb77b29
Uri Ziv <uri.ziv1@ibm.com>
2025-12-28 17:40:37 +0200

    debug

--- controllers/servers/host_definer/watcher/host_definition_watcher.py
+++ controllers/servers/host_definer/watcher/host_definition_watcher.py
@@ -18,7 +18,8 @@ class HostDefinitionWatcher(Watcher):
         self._watch_host_definition_with_timeout('')
         while self._loop_forever():
             resource_version = self._get_k8s_object_resource_version(self.host_definitions_api.get())
-            logger.info("self.host_definitions_api.get(): %s", self.host_definitions_api.get())
+            # logger.info("self.host_definitions_api.get(): %s", self.host_definitions_api.get())
+            # ResourceInstance[HostDefinitionList]  #  kc get hostdefinitions.csi.ibm.com
             logger.info(resource_version)
             logger.info("Debug - uriziv - 70")
             self._watch_host_definition_with_timeout(resource_version)
