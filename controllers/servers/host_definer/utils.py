b6237cfcb94edb20099994bb077d06f5c072c6fd
Uri Ziv <uri.ziv1@ibm.com>
2026-01-13 15:10:59 +0200

    pylint

--- controllers/servers/host_definer/utils.py
+++ controllers/servers/host_definer/utils.py
@@ -8,9 +8,10 @@ def get_node_initiators_from_node(node_name):
     """docstring"""
     core_api = client.CoreV1Api()
     k8s_node = core_api.read_node(name=node_name)
-    node_annotations = k8s_node.metadata.annotations 
+    node_annotations = k8s_node.metadata.annotations
     node_initiators = node_annotations.get(common_settings.NODE_INITIATORS_FIELD, "{}")
     return node_initiators
+
 
 def generate_io_group_from_labels(labels):
     """docstring"""
