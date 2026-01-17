64421d47ba1f11f24308ce719e3c874e25b11ea7
Uri Ziv <uri.ziv1@ibm.com>
2026-01-17 23:39:04 +0200

    Update _get_host_definition_manifest to update node_initiators from cache NODES.

--- controllers/servers/host_definer/watcher/watcher_helper.py
+++ controllers/servers/host_definer/watcher/watcher_helper.py
@@ -159,7 +159,7 @@ class Watcher(KubernetesManager):
             settings.METADATA: {
                 common_settings.NAME_FIELD: host_definition_info.name,
                 common_settings.ANNOTATIONS_FIELD: {
-                    common_settings.NODE_INITIATORS_FIELD: host_definition_info.node_initiators
+                    common_settings.NODE_INITIATORS_FIELD: NODES[host_definition_info.node_name].node_initiators
                     }
             },
             settings.SPEC: {
