ecde2738c73e66ae57da80ad4b571c1d73e75ba3
Uri Ziv <uri.ziv1@ibm.com>
2026-01-26 10:55:43 +0200

    remred

--- controllers/servers/host_definer/utils.py
+++ controllers/servers/host_definer/utils.py
@@ -1,16 +1,6 @@
 """docstring"""
-from kubernetes import client
 import controllers.servers.host_definer.settings as host_definer_settings
 import controllers.common.settings as common_settings
-
-
-def get_node_initiators_from_node(node_name):
-    """docstring"""
-    core_api = client.CoreV1Api()
-    k8s_node = core_api.read_node(name=node_name)
-    node_annotations = k8s_node.metadata.annotations
-    node_initiators = node_annotations.get(common_settings.NODE_INITIATORS_FIELD, "{}")
-    return node_initiators
 
 
 def generate_io_group_from_labels(labels):
