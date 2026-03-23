d3ae7cf86b3d9d0a3ca5906a9761b64c341311d9
Eyal Perek <eyalp@il.ibm.com>
2026-03-23 07:52:12 +0200

    comp

--- node/pkg/driver/device_connectivity/device_connectivity_helper_scsigeneric.go
+++ node/pkg/driver/device_connectivity/device_connectivity_helper_scsigeneric.go
@@ -2226,7 +2226,6 @@ func (o *OsDeviceConnectivityHelperGeneric) GetHostsIdByArrayIdentifiers(arrayId
 				if err == nil {
 					logger.Debugf("portState path (%s) was found. Adding host ID {%v} to the id list", idPath, hostNum)
 					activeHosts[hostNum] = true
-				}
 				} else {
 					logger.Warningf("Host number in for target file was not valid : {%v}", idPath)
 				}
@@ -3530,7 +3529,6 @@ func (o GetDmsPathHelperGeneric) validateDMIntegrity(dmPath string) (string, err
 	return "", fmt.Errorf("dm device %s has slaves but none are in 'running' state", dmName)
 }
 
-func normalizeWWID(raw string) string {
 func normalizeWWID(raw string) string {
 	// 1. Lowercase and clean whitespace/newlines
 	s := strings.ToLower(strings.TrimSpace(raw))
