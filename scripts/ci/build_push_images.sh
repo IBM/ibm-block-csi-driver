e148ab2657cc5dc3c3e4811590b263be8f2ddec7
Shlomit Neufeld <shlomitn@il.ibm.com>
2026-04-14 15:12:31 +0300

    trial to speed up build, specifically in zLinux

--- scripts/ci/build_push_images.sh
+++ scripts/ci/build_push_images.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -xe
 
+# Enable Docker BuildKit for cache mount support
+export DOCKER_BUILDKIT=1
+
 # Validations
 MANDATORY_ENVS="IMAGE_VERSION BUILD_NUMBER DOCKER_REGISTRY CSI_NODE_IMAGE CSI_CONTROLLER_IMAGE CSI_HOST_DEFINER_IMAGE GIT_BRANCH"
-for envi in $MANDATORY_ENVS; do 
+for envi in $MANDATORY_ENVS; do
     [ -z "${!envi}" ] && { echo "Error - Env $envi is mandatory for the script."; exit 1; } || :
 done
 
--- scripts/run_static_code_analysis.sh
+++ scripts/run_static_code_analysis.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -x
 
+# Enable Docker BuildKit for cache mount support
+export DOCKER_BUILDKIT=1
+
 docker build -f Dockerfile-controllers.test -t csi-controller-tests . || exit 1
 
 EXIT_STATUS=0
