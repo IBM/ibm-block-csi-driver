8fb513766ba74e25aabf8fc9923d80dc2268323b
Shlomit Neufeld <shlomitn@il.ibm.com>
2026-04-16 09:56:25 +0300

    avoid segmentation fault for z server

--- controllers/scripts/unitests.sh
+++ controllers/scripts/unitests.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-set -x
+set -xe
 coveragedir=/driver/coverage/
 [ ! -d $coveragedir ] && mkdir -p $coveragedir
-exec nosetests --exe --with-coverage --cover-xml --cover-xml-file=$coveragedir/.coverage.xml --cover-package=common --cover-package=controllers --with-xunit --xunit-file=$coveragedir/.unitests.xml $@
+
+# Use pytest instead of nose (nose is unmaintained and has issues on s390x)
+# pytest is more stable and better maintained
+# -n 4: Run tests in parallel using 4 processes (equivalent to nose --processes=4)
+# --timeout=600: Set test timeout to 600 seconds (equivalent to nose --process-timeout=600)
+pytest \
+    --verbose \
+    --capture=no \
+    --tb=short \
+    -n 4 \
+    --timeout=600 \
+    --cov=common \
+    --cov=controllers \
+    --cov-report=xml:$coveragedir/.coverage.xml \
+    --junit-xml=$coveragedir/.unitests.xml \
+    controllers/tests/ \
+    $@
+
+echo "Unit tests completed successfully"
