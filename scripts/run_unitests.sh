3092ecb4c4688669f043fd61c46376f83461bbea
Eyal Ben-David <bdeyal@gmail.com>
2026-04-30 01:23:45 +0300

    exec with py3.12, use docker if avail in unittests

--- scripts/run_unitests.sh
+++ scripts/run_unitests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 [ -n "$1" ] && coverage="-v $1:/driver/coverage:z"
-podman build -f Dockerfile-controllers.test -t csi-controller-tests . && \
-podman run --entrypoint ./controllers/scripts/unitests.sh --rm -t $coverage csi-controller-tests
+docker build -f Dockerfile-controllers.test -t csi-controller-tests . && \
+docker run --entrypoint ./controllers/scripts/unitests.sh --rm -t $coverage csi-controller-tests
