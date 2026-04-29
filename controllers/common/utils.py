6f232a7f08e62d7336102ce367bef5650ea3388e
Eyal Ben David <eyalbe@il.ibm.com>
2026-04-29 18:22:54 +0300

    Work in progress for python-312 ubi

--- controllers/common/utils.py
+++ controllers/common/utils.py
@@ -25,7 +25,7 @@ def set_current_thread_name(name):
     """
     if name:
         current_thread = threading.current_thread()
-        current_thread.setName(name)
+        current_thread.name = name
 
 
 def string_to_array(str_val, separator):
