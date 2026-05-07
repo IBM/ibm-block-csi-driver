10d9b7ffa57901df4d25ff35a15e4f7163db520d
Sushil Bhile <sushil.bhile@ibm.com>
2026-05-07 11:28:09 +0530

    Reverting pytest from unitests.sh

--- controllers/scripts/unitests.sh
+++ controllers/scripts/unitests.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-set -ex
-
-export COVERAGE_NO_CTRACER=1
-
+set -x
 coveragedir=/driver/coverage/
-[ ! -d "$coveragedir" ] && mkdir -p "$coveragedir"
-
-exec pytest \
-  --verbose \
-  --capture=no \
-  --tb=short \
-  --cov=common \
-  --cov=controllers \
-  --cov-report=xml:$coveragedir/.coverage.xml \
-  --junit-xml=$coveragedir/.unitests.xml \
-  controllers/tests/ "$@"
+[ ! -d $coveragedir ] && mkdir -p $coveragedir
+exec nosetests --exe --with-coverage --cover-xml --cover-xml-file=$coveragedir/.coverage.xml --cover-package=common --cover-package=controllers --with-xunit --xunit-file=$coveragedir/.unitests.xml $@
