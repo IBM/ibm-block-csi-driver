b909b9a5db31a81c1bfdf192a9d31ffca5521132
Eyal Perek <eyalp@il.ibm.com>
2026-05-07 06:12:03 +0300

    fix MountSensitive

--- node/pkg/driver/mount/mount_wrapper.go
+++ node/pkg/driver/mount/mount_wrapper.go
@@ -206,6 +206,10 @@ func (m *Mounter) MountSensitive(source, target, fstype string, options, sensiti
     } else {
         logger.Infof("Mounting %s to %s with options %v", source, target, options)
     }
+	
+    if err := os.MkdirAll(target, 0750); err != nil {
+        return fmt.Errorf("failed to create target directory %s: %v", target, err)
+    }	
 
     // 2. Combine all options for the system call
     allOptions := append(options, sensitiveOptions...)
