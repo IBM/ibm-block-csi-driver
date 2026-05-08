8030ce60b179ba01e474bf804687f5e0b1ded9d8
Sushil Bhile <sushil.bhile@ibm.com>
2026-05-08 10:03:58 +0530

    updated unittest to run according to architecture

--- controllers/scripts/unitests.sh
+++ controllers/scripts/unitests.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -ex
 
+# REQUIRED: ensure protobuf fix is applied
+if [[ "$(uname -m)" == "s390x" ]]; then
+  export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+fi
+
 coveragedir=/driver/coverage/
 [ ! -d "$coveragedir" ] && mkdir -p "$coveragedir"
 
-pytest --verbose --capture=no --tb=short -n 4 --timeout=600 --cov=common --cov=controllers --cov-report=xml:/driver/coverage//.coverage.xml --junit-xml=/driver/coverage//.unitests.xml controllers/tests/
+pytest --verbose --capture=no --tb=short -n 4 --timeout=600 \
+  --cov=common --cov=controllers \
+  --cov-report=xml:/driver/coverage/.coverage.xml \
+  --junit-xml=/driver/coverage/.unitests.xml \
+  controllers/tests/
