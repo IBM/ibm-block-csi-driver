3121a62005a67a939c46d3acfa3c43d1f786c18f
Parthraj Ghatge <parthghatge982@gmail.com>
2026-05-14 02:00:12 +0530

    Add two new fields in ReplicationInfo i.e. replication_status & status_message.
    Signed-off-by: Parthraj Ghatge <parthghatge982@gmail.com>

--- controllers/servers/utils.py
+++ controllers/servers/utils.py
@@ -1178,3 +1178,13 @@ def is_call_home_enabled():
 
 def get_odf_call_home_version():
     return getenv(servers_settings.ODF_VERSION_FOR_CALL_HOME_ENV_VAR, '')
+
+
+def getattr_as_str(obj, attr, default=servers_settings.UNKNOWN):
+    value = getattr(obj, attr, None)
+
+    if value is None:
+        return default
+
+    value = str(value).strip()
+    return value or default
