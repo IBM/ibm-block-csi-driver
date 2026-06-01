cfbf561647e62cdca4a3361620611258b03bf382
Sushil Bhile <sushilmaxbhile@gmail.com>
2026-06-01 12:16:44 +0530

    sending replication.volume_group_id to self._demote_ear_replication_volume
    Signed-off-by: Sushil Bhile <sushilmaxbhile@gmail.com>
    Signed-off-by: Sushil Bhile <sushilsanjaybhile@gmail.com>

--- controllers/array_action/array_mediator_svc.py
+++ controllers/array_action/array_mediator_svc.py
@@ -2183,7 +2183,7 @@ class SVCArrayMediator(ArrayMediatorAbstract, VolumeGroupInterface):
         if replication.replication_type == array_settings.REPLICATION_TYPE_MIRROR:
             self._demote_replication_volume(replication.name)
         elif replication.replication_type == array_settings.REPLICATION_TYPE_EAR:
-            self._demote_ear_replication_volume(replication.name)
+            self._demote_ear_replication_volume(replication.volume_group_id)
 
     def _demote_replication_volume(self, replication_name):
         rcrelationship = self._get_rcrelationship_by_name(replication_name)
