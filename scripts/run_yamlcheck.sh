7ea8fa72690294bed8f50ef380f6f6bc44ec6a74
Shlomit Neufeld <shlomitn@il.ibm.com>
2026-07-21 08:40:44 +0300

    kubeconform is the maintained successor to kubeval

--- scripts/run_yamlcheck.sh
+++ scripts/run_yamlcheck.sh
@@ -4,7 +4,10 @@ set -x
 arch=$(uname -m)
 if [[ "$arch" == "x86"* ]];
 then
-docker run -t -v `pwd`/deploy/kubernetes/examples:/deploy/kubernetes/examples garethr/kubeval --ignore-missing-schemas deploy/kubernetes/examples/*.yaml
+  KUBECONFORM_VERSION=$(curl -s https://api.github.com/repos/yannh/kubeconform/releases/latest | python3 -c "import sys,json; print(json.load(sys.stdin)['tag_name'])")
+  curl -sSL "https://github.com/yannh/kubeconform/releases/download/${KUBECONFORM_VERSION}/kubeconform-linux-amd64.tar.gz" | tar -xz -C /tmp kubeconform
+  /tmp/kubeconform -ignore-missing-schemas deploy/kubernetes/examples/*.yaml
+  rm -f /tmp/kubeconform
 else
-echo "Skipping yaml validations stage. garethr/kubeval only supports x86"
+  echo "Skipping yaml validations stage. kubeconform only supports x86"
 fi
