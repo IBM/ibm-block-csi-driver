cbfb795e9d975975ec6e862b65b1c17520f38830
csi-odf <csi-odf@csijen-worker1.xiv.ibm.com>
2026-08-13 10:06:00 +0300

    comp

--- node/pkg/driver/device_connectivity/device_connectivity_helper_scsigeneric.go
+++ node/pkg/driver/device_connectivity/device_connectivity_helper_scsigeneric.go
@@ -8464,7 +8464,7 @@ const (
        NotNVMe       NvmeType = "non-nvme"
 )
 
-func (r *OsDeviceConnectivityHelperScsiGeneric) DevicesAreNvme(ctx context.Context, device string) (NvmeType, error) {
+func DevicesAreNvme(ctx context.Context, gater *executer.KeyedGater, device string) (NvmeType, error) {
 	if err := ctx.Err(); err != nil {
 		return NotNVMe, err
 	}
