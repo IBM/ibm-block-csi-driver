0e1b664092fbcc97fab853f4025a856892a9d6ac
csi-odf <csi-odf@csijen-worker1.xiv.ibm.com>
2026-09-16 07:32:56 +0300

    comp

--- node/pkg/driver/device_connectivity/device_connectivity_helper_scsigeneric.go
+++ node/pkg/driver/device_connectivity/device_connectivity_helper_scsigeneric.go
@@ -7618,8 +7618,7 @@ func (of *GetDmsPathHelperGeneric) safeSettle(ctx context.Context, gater *execut
 				
 				if readErr != nil {
 					logger.Warningf("safeSettle open device %s failed with read err %v", targetDeviceNode, readErr)
-				}
-				else {
+				} else {
 					logger.Warningf("safeSettle open device %s success", targetDeviceNode)
 				}
 				
